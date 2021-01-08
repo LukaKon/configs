06479cb797755103dbac21f93f4bea1a95a44117
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-01-08 23:54:57 +0100

    added to snapshot.py function to delete old snapshots

--- scripts/snapshot.py
+++ scripts/snapshot.py
@@ -2,8 +2,10 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i python3 -p "python3.withPackages(ps: [ps.colorama])"
 """
+import os
+import re
 import subprocess
-from datetime import datetime
+from datetime import datetime, timedelta
 
 data = datetime.now().strftime('%Y-%m-%d_%H-%M')
 
@@ -17,8 +19,36 @@ paths = (
     {'name': 'root', 'directory': '/', 'snap_loc': f'/.snapshots/root-{data}'},
 )
 
-print('\nCreate snapshots:\n')
 
-for x in paths:
-    print(f"{x['name']}: ", subprocess.run(['sudo', 'btrfs', 'subvolume',
-                                            'snapshot', x['directory'], x['snap_loc']]).returncode)
+def create_new_snaps():
+    for snap in paths:
+        print(f"{snap['name']}:")
+        subprocess.run(['sudo', 'btrfs', 'subvolume',
+                        'snapshot', snap['directory'], snap['snap_loc']]).returncode
+        print('\n')
+
+
+def delete_old_snaps():
+    for folder in paths:
+        print(f"\n\nDelete from: {folder['directory']}")
+
+        path = f"{folder['directory']}/.snapshots"
+        expression_date = '[0-9]{4}-[0-9]{2}-[0-9]{2}'
+
+        with os.scandir(path) as snaps:
+
+            for entry in snaps:
+                match = re.search(expression_date, entry.name)
+                # convert 'match' (str) to date
+                match_date = datetime.strptime(match.group(), '%Y-%m-%d')
+
+                if match_date < datetime.today() - timedelta(days=7):   # selecting folders older than 7 days old
+                    print(f'{entry.name}: deleted')
+                    subprocess.run(
+                        ['sudo', 'rm', '-r', f'{path}/{entry.name}'])
+
+
+print('\nCreate new snapshots:\n')
+create_new_snaps()
+print('\nDelete old snapshots:\n')
+delete_old_snaps()
