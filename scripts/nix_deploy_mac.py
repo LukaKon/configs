a972784ca090e1462cd26e3c979bf9efea681405
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-01-13 14:58:48 +0100

    mac mod snaps script and aliases

--- scripts/nix_deploy_mac.py
+++ scripts/nix_deploy_mac.py
@@ -3,12 +3,16 @@ Copying configuration files from folder where they were edited to target folder 
 """
 import os
 import subprocess
+import getpass
 
-nix_edit_folder = '/home/lk/configs/nix_mac/'
+user = getpass.getuser()
+
+nix_edit_folder = f'/home/{user}/configs/nix_mac/'
 nix_target_folder = '/etc/nixos/'
 
 print(f'\nCopying config files to {nix_target_folder}...')
-subprocess.run(['sudo', 'rsync', '-rav', '--delete', output_folder, nix_target_folder])
+subprocess.run(['sudo', 'rsync', '-rav', '--delete',
+                nix_edit_folder, nix_target_folder])
 
 print('\nRebuild configuration...')
 subprocess.run(['sudo', 'nixos-rebuild', 'switch'])
