b8a3d041bd955e5431f71b9bdeb4ea0467428bd2
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-01-14 20:34:13 +0100

    update Steam config

--- nix/network.nix
+++ nix/network.nix
@@ -14,13 +14,13 @@
   # programs.mtr.enable = true;
 
   networking = {
-                hostName = "fuji"; # Define your hostname.
-                # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-                networkmanager.enable = true;
+                  hostName = "fuji"; # Define your hostname.
+                  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+                  networkmanager.enable = true;
 
-          interfaces.wlp1s0.ipv4.addresses = [{
-                address = "192.168.0.3";
-                prefixLength = 24;
+                  interfaces.wlp1s0.ipv4.addresses = [{
+                        address = "192.168.0.3";
+                        prefixLength = 24;
               }];
 
           defaultGateway = "192.168.0.1";
