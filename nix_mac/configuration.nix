9ef83e56298b41dc1a15ad7a122a8b535d2515e3
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-01-17 23:14:36 +0100

    mac gnome

--- nix_mac/configuration.nix
+++ nix_mac/configuration.nix
@@ -13,8 +13,9 @@
       # ./services/desktops/qtile.nix	# Qtile window manager settings
       # ./services/desktops/plasma.nix	# Plasma window manager settings
       # ./services/desktops/lxqt.nix	# LXQT window manager settings
-      ./services/desktops/mate.nix	# Mate window manager settings
-      ./services/keyring.nix
+      ./services/desktops/gnome.nix	# Gnome window manager settings
+      # ./services/desktops/mate.nix	# Mate window manager settings
+      # ./services/keyring.nix
       ./services/laptop.nix   # settings for laptop
       ./programs/zsh.nix	# zsh and console settings
       ./fonts.nix		# Fonts to install
@@ -23,5 +24,6 @@
       ./shellAliases.nix	# Shell aliases
       ./virtualisation/docker.nix	# Docker 
       ./nvim/nvim.nix		# TODO nvim configuration in separate files
+      ./flatpak.nix
     ];
 }
