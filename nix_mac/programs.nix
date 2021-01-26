5f710df8d9845376efa06412fb1cd8594d3ea3b2
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-01-26 21:17:10 +0100

    update python to 3.8

--- nix_mac/programs.nix
+++ nix_mac/programs.nix
@@ -72,7 +72,7 @@
 
                         psutils
 
-                        (python37.withPackages(ps: with ps;
+                        (python38.withPackages(ps: with ps;
                         [
                             autopep8
                             flake8
