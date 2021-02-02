cf42a30bff45df75b002123aa35ffd58de24c02e
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-02-02 19:38:07 +0100

    up

--- nix/services/postgresql.nix
+++ nix/services/postgresql.nix
@@ -2,21 +2,21 @@
 
 {
 	services.postgresql = {
-								enable = true;
-								package = pkgs.postgresql_12;
-								enableTCPIP = true;
-								# dataDir = "/home/lk/postDB";
-								authentication = pkgs.lib.mkOverride 12 ''
-								local all all trust
-								host all all localhost trust
-								host all all ::1/128 trust
-								host all all 192.168.0.0/24 trust
-								'';
-								initialScript = pkgs.writeText "backend-initScript" ''
-								CREATE ROLE lko WITH LOGIN PASSWORD 'Lukasz82';
+							enable = true;
+							package = pkgs.postgresql_12;
+							enableTCPIP = true;
+							# dataDir = "/home/lk/postDB";
+							authentication = pkgs.lib.mkOverride 12 ''
+							local all all trust
+							host all all localhost trust
+							host all all ::1/128 trust
+							host all all 192.168.0.0/24 trust
+							'';
+							initialScript = pkgs.writeText "backend-initScript" ''
+							CREATE ROLE lko WITH LOGIN PASSWORD 'Lukasz82';
 
-								CREATE DATABASE lko;
-								GRANT ALL PRIVILEGES ON DATABASE lko TO lko;
-								'';
-                        };
+							CREATE DATABASE lko;
+							GRANT ALL PRIVILEGES ON DATABASE lko TO lko;
+							'';
+						};
 }
--- nix_mac/configuration.nix
+++ nix_mac/configuration.nix
@@ -2,28 +2,28 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./system.nix		# Base system settings
-      ./network.nix		# Network configuration
-      ./programs.nix		# List of installed programs
-      ./security/doas.nix	# Add 'doas'
-      ./services/openssh.nix	# SSH
-      ./services/postgresql.nix	# PostgreSQL
-      # ./services/desktops/qtile.nix	# Qtile window manager settings
-      # ./services/desktops/plasma.nix	# Plasma window manager settings
-      # ./services/desktops/lxqt.nix	# LXQT window manager settings
-      # ./services/desktops/gnome.nix	# Gnome window manager settings
-      ./services/desktops/mate.nix	# Mate window manager settings
-      ./services/keyring.nix
-      ./services/laptop.nix   # settings for laptop
-      ./programs/zsh.nix	# zsh and console settings
-      ./fonts.nix		# Fonts to install
-      ./nixpkgs/nixpkgs.nix
-      ./users.nix		# Users configurations
-      ./shellAliases.nix	# Shell aliases
-      ./virtualisation/docker.nix	# Docker
-      ./nvim/nvim.nix		# TODO nvim configuration in separate files
-      ./flatpak.nix
-    ];
+			[ # Include the results of the hardware scan.
+			./hardware-configuration.nix
+			./system.nix		# Base system settings
+			./network.nix		# Network configuration
+			./programs.nix		# List of installed programs
+			./security/doas.nix	# Add 'doas'
+			./services/openssh.nix	# SSH
+			./services/postgresql.nix	# PostgreSQL
+			# ./services/desktops/qtile.nix	# Qtile window manager settings
+			# ./services/desktops/plasma.nix	# Plasma window manager settings
+			# ./services/desktops/lxqt.nix	# LXQT window manager settings
+			# ./services/desktops/gnome.nix	# Gnome window manager settings
+			./services/desktops/mate.nix	# Mate window manager settings
+			./services/keyring.nix
+			./services/laptop.nix   # settings for laptop
+			./programs/zsh.nix	# zsh and console settings
+			./fonts.nix		# Fonts to install
+			./nixpkgs/nixpkgs.nix
+			./users.nix		# Users configurations
+			./shellAliases.nix	# Shell aliases
+			./virtualisation/docker.nix	# Docker
+			./nvim/nvim.nix		# TODO nvim configuration in separate files
+			./flatpak.nix
+			];
 }
