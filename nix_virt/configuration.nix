fc4900d888efb585568a5e48283f16e399352256
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-02-13 12:57:52 +0100

    virt nginx

--- nix_virt/configuration.nix
+++ nix_virt/configuration.nix
@@ -12,6 +12,7 @@
 				./security/doas.nix	# Add 'doas'
 				./services/openssh.nix	# SSH
 				./services/postgresql.nix	# PostgreSQL
+				./services/nginx.nix
 				# ./services/desktops/mate.nix	# mate window manager settings
 				./services/keyring.nix
 				./services/remote.nix	# remote server/desktop
