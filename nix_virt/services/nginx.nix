23a3a6f32ee531ea45f3aafc20f62079881fbff5
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-02-13 13:34:41 +0100

    nginx update

--- nix_virt/services/nginx.nix
+++ nix_virt/services/nginx.nix
@@ -3,9 +3,16 @@
 {
     services.nginx.enable = true;
     services.nginx.virtualHosts."luka.org" = {
-                addSSL = true;
-                enableACME = true;
-                forceSSL=true;
+                #addSSL = true;
+                #enableACME = true;
+                # forceSSL=true;
                 root = "/var/www/luka.org";
                 };
+    
+                #security.acme = {
+                #    acceptTerms = true;
+                #    certs = {"luka.org".email = "lukasz.konieczny.lk@gmail.com";};
+                    
+                #};
 }
+
