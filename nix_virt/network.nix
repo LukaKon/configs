adc592f597f4a4785d4f96a5b129ae3256019e27
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-02-13 12:10:41 +0100

    virt network mod

--- nix_virt/network.nix
+++ nix_virt/network.nix
@@ -14,16 +14,16 @@
   # programs.mtr.enable = true;
 
   networking = {
-                  hostName = "virt"; # Define your hostname.
-                  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-                  networkmanager.enable = true;
+					hostName = "virt"; # Define your hostname.
+					# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+					# networkmanager.enable = true;
 
-                #   interfaces.wlp1s0.ipv4.addresses = [{
-						# address = "192.168.0.3";
-						# prefixLength = 24;
-				#   }];
+					interfaces.enp0s3.ipv4.addresses = [{
+					address = "192.168.0.5";
+					prefixLength = 24;
+					}];
 
-					#   defaultGateway = "192.168.0.1";
-					#   nameservers = ["37.8.214.2" "31.11.202.254"];
+					defaultGateway = "192.168.0.1";
+					nameservers = ["37.8.214.2" "31.11.202.254"];
 				};
 }
