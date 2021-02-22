217a1c9f6054586ee57af63c4c8fe20ae12eaa7d
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-02-22 19:12:58 +0100

    virt mate

--- nix_virt/services/remote.nix
+++ nix_virt/services/remote.nix
@@ -2,6 +2,6 @@
 
 {
     services.xrdp.enable = true;
-    services.xrdp.defaultWindowManager = "${pkgs.icewm}/bin/icewm";
+    # services.xrdp.defaultWindowManager = "${pkgs.icewm}/bin/icewm";
     networking.firewall.allowedTCPPorts = [ 3389 ];
 }
