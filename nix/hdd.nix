7ecd1d68d912102d038ff9f462c8af7ec8605be4
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-03-06 14:53:56 +0100

    disk repleace

--- nix/hdd.nix
+++ nix/hdd.nix
@@ -6,8 +6,14 @@
                       fsType = "btrfs";
                       options = ["subvol=@data"];
                   };
+  # fileSystems."/home/lk/vm" = {
+  #                     device = "/dev/disk/by-uuid/2f8299dd-bba5-4256-81cf-516828cc910b";
+  #                     fsType = "btrfs";
+  #                     options = ["subvol=@vm"];
+  #                 };
+
   fileSystems."/home/lk/vm" = {
-                      device = "/dev/disk/by-uuid/2f8299dd-bba5-4256-81cf-516828cc910b";
+                      device = "/dev/disk/by-uuid/ef94fe71-c21b-4da8-aabb-e80588efc941";
                       fsType = "btrfs";
                       options = ["subvol=@vm"];
                   };
