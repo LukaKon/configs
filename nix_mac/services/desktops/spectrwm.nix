2f2bb91f78139b6cf684b0457adde94aace2143a
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-04-17 20:23:26 +0200

    up mac

---
+++ nix_mac/services/desktops/spectrwm.nix
@@ -0,0 +1,41 @@
+
+{config, lib, pkgs, ...}:
+
+{
+	services = {
+				xserver = {
+							enable = true;
+
+							# videoDrivers = ["modesetting" "nvidia"];
+							# displayManager.startx.enable = true;  # necessary to
+							# create .xinitrc file
+
+							# i3
+
+							# displayManager.lightdm.enable = true;
+							# displayManager.defaultSession = "none+i3";
+
+							desktopManager.xterm.enable = false;
+
+							windowManager.spectrwm = {
+											enable = true;
+											# package = pkgs.i3-gaps;
+											# configFile = ./i3.conf;
+											# extraPackages = with pkgs; [
+											# dmenu
+											# i3status
+											# i3lock
+											# i3blocks
+											# ];
+							};
+
+							layout = "pl";
+						};
+
+				autorandr.enable = true;
+			};
+	environment = {
+                    systemPackages = with pkgs;
+                    [xlockmore];
+				};
+}
