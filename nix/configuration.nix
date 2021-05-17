2d7cc8f3dfc35fb2c88fb90e24971fc6cfdbe528
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-05-17 19:05:57 +0200

    nix qtile again failing config file

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -14,11 +14,11 @@
 				./security/doas.nix	# Add 'doas'
 				./services/openssh.nix	# SSH
 				./services/postgresql.nix	# PostgreSQL
-				# ./services/desktops/qtile.nix	# Qtile window manager settings
+				./services/desktops/qtile.nix	# Qtile window manager settings
 				# ./services/desktops/i3/i3.nix	# i3 window manager settings
 				# ./services/desktops/gnome/gnome.nix	# Gnome window manager settings
 				./services/desktops/spectrwm.nix
-				./services/desktops/xmonad/xmonad.nix
+				# ./services/desktops/xmonad/xmonad.nix
 				./services/keyring.nix
 				# ./services/bluetooth.nix
 				./services/remote.nix	# remote server/desktop
