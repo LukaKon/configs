e4aef15efc5aea211a36f282b19bbdedd083a3d5
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-05-22 18:01:18 +0200

    nfs

--- nix/nfs.nix
+++ nix/nfs.nix
@@ -1,18 +1,24 @@
 {config, ...}:
 
 {
-    fileSystems."/export/data" = {
-        device = "/home/lk/data/programing";
-        options = ["bind" "nfsvers=4.2" "x-systemd.automount" "noauto"];
-    };
+    # fileSystems."/export/data" = {
+    #     device = "/home/lk/data/programing";
+    #     options = ["bind" "nfsvers=4.2" "x-systemd.automount" "noauto"];
+    # };
 
-    services.nfs.server = {
-                            enable = true;
-                            exports = ''
-                                    /export             192.168.1.10(rw,fsid=0,no_subtree_check,async)
-                                    /export/data        192.168.1.10(rw,nohide,insecure,no_subtree_check,async)
-                            '';
+    # services.nfs.server = {
+    #                         enable = true;
+    #                         exports = ''
+    #                                 /export             192.168.1.10(rw,fsid=0,no_subtree_check,async)
+    #                                 /export/data        192.168.1.10(rw,nohide,insecure,no_subtree_check,async)
+    #                         '';
 
+    # };
+    fileSystems."/home/lk/rasp" = {
+        device = "192.168.1.9:/share";
+        fsType = "nfs";
+        # options = ["x-systemd.idle-timeout=600"];
+        options = ["nfsvers=4.2" "x-systemd.automount" "noauto" ];
     };
 
     networking.firewall.allowedTCPPorts = [2049];
