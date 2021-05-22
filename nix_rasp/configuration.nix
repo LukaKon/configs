b9bdc070666f26e61c48707eb1e12c7771860c34
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-05-22 21:18:38 +0200

    up rasp spectrwm

--- nix_rasp/configuration.nix
+++ nix_rasp/configuration.nix
@@ -14,9 +14,9 @@
 				./services/openssh.nix	# SSH
 				./services/postgresql.nix	# PostgreSQL
 				# ./services/desktops/i3/i3.nix	# i3 window manager settings
-				# ./services/desktops/spectrwm.nix
+				./services/desktops/spectrwm.nix
 				# ./services/desktops/qtile.nix
-				./services/desktops/mate.nix
+				# ./services/desktops/mate.nix
 				./services/keyring.nix
 				./services/remote.nix	# remote server/desktop
 				./services/cron.nix
--- nix_rasp/services/desktops/spectrwm.nix
+++ nix_rasp/services/desktops/spectrwm.nix
@@ -6,27 +6,16 @@
 				xserver = {
 							enable = true;
 
-							# videoDrivers = ["modesetting" "nvidia"];
 							# displayManager.startx.enable = true;  # necessary to
 							# create .xinitrc file
 
-							# i3
-
-							# displayManager.lightdm.enable = true;
+							displayManager.lightdm.enable = true;
 							displayManager.defaultSession = "none+spectrwm";
 
 							desktopManager.xterm.enable = false;
 
 							windowManager.spectrwm = {
 											enable = true;
-											# package = pkgs.i3-gaps;
-											# configFile = ./i3.conf;
-											# extraPackages = with pkgs; [
-											# dmenu
-											# i3status
-											# i3lock
-											# i3blocks
-											# ];
 							};
 
 							layout = "pl";
