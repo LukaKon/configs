e269d528c5780962d4adfe7fe23d6fff8ccbaa54
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-05-25 20:06:41 +0200

    remote up

--- nix_rasp/services/remote.nix
+++ nix_rasp/services/remote.nix
@@ -7,6 +7,6 @@
     services.xrdp.enable = true;
     # services.xrdp.defaultWindowManager = "${pkgs.openbox}/bin/openbox";
     # services.xrdp.defaultWindowManager = "${pkgs.icewm}/bin/icewm";
-    services.xrdp.defaultWindowManager = "mate";
+    services.xrdp.defaultWindowManager = "mate-session";
     networking.firewall.allowedTCPPorts = [ 3389 ];
 }
