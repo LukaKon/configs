90d3aaa4f44b2e5ea2e2765e9fd3a30f2c23c9e5
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-06-04 11:16:54 +0200

    mac up

--- nix_mac/configuration.nix
+++ nix_mac/configuration.nix
@@ -6,12 +6,12 @@
 				./hardware-configuration.nix
 				./system.nix		# Base system settings
 				./network.nix		# Network configuration
-				# ./programs.nix		# List of installed programs
+				./programs.nix		# List of installed programs
 				./sound.nix
 				./locale.nix
 				./security/doas.nix	# Add 'doas'
 				./services/openssh.nix	# SSH
-				# ./programs/postgresql.nix	# PostgreSQL
+				./programs/postgresql.nix	# PostgreSQL
 				# ./desktops/qtile.nix	# Qtile window manager settings
 				# ./desktops/kde.nix
 				# ./desktops/spectrwm.nix
@@ -23,12 +23,12 @@
 				./services/laptop.nix   # settings for laptop
 				# ./services/remote.nix
 				./programs/zsh.nix	# zsh and console settings
-				# ./fonts.nix		# Fonts to install
+				./fonts.nix		# Fonts to install
 				./nixpkgs.nix
 				./users.nix		# Users configurations
 				./shellAliases.nix	# Shell aliases
-				# ./virtualisation/docker.nix	# Docker
-				# ./programs/nvim.nix		# TODO nvim configuration in separate files
-				# ./services/flatpak.nix
+				./virtualisation/docker.nix	# Docker
+				./programs/nvim.nix		# TODO nvim configuration in separate files
+				./services/flatpak.nix
 			];
 }
