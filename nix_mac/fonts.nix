b5169ceade640ddd69313f61673552bd5382ffcf
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-06-28 20:41:57 +0200

    up

--- nix_mac/fonts.nix
+++ nix_mac/fonts.nix
@@ -1,22 +1,20 @@
 {config, pkgs, ...}:
 
 {
-	fonts = {
-				fonts = with pkgs; [
-									inconsolata
-									fira-code
-									fira-code-symbols
-									ubuntu_font_family
-									corefonts
-									noto-fonts-emoji
-									# joypixels
-									nerdfonts
-								];
-				fontconfig = {
-								defaultFonts = {
-												monospace = ["Fira Code"];
-												# emoji = ["Joypixels"];
-											};
-		};
-	};
+  fonts = {
+    fonts = with pkgs; [
+      inconsolata
+      fira-code
+      fira-code-symbols
+      ubuntu_font_family
+      corefonts
+      noto-fonts-emoji
+      nerdfonts
+    ];
+    fontconfig = {
+      defaultFonts = {
+        monospace = ["Fira Code"];
+      };
+    };
+  };
 }
