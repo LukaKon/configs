ba93d4bf6a967c9efddfd1151c22bfc6dbb9cad3
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-07-12 17:32:10 +0200

    rem nix_virt

--- nix_virt/services/remote.nix
+++
@@ -1,7 +0,0 @@
-{config, pkgs, ...}:
-
-{
-    services.xrdp.enable = true;
-    # services.xrdp.defaultWindowManager = "${pkgs.icewm}/bin/icewm";
-    networking.firewall.allowedTCPPorts = [ 3389 ];
-}
