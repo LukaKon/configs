87d0a514ffc3963addc7806309c1778992f6e6a7
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-07-13 19:08:51 +0200

    mac -> kde

--- nix_mac/desktops/kde.nix
+++ nix_mac/desktops/kde.nix
@@ -2,16 +2,15 @@
 {config, lib, ...}:
 
 {
-	services.xserver = {
-                        enable = true;
-                        videoDrivers = ["modesetting"]; #["nouveau"];
-                        # displayManager.startx.enable = true;  # necessary to
-                        # create .xinitrc file
+  services.xserver = {
+    enable = true;
+    videoDrivers = ["modesetting"]; #["nouveau"];
+    # displayManager.startx.enable = true;  # necessary to
+    # create .xinitrc file
 
-                        # Mate
-                        displayManager.sddm.enable = true;
-                        desktopManager.plasma5.enable = true;
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
 
-                        layout = "pl";
-                      };
+    layout = "pl";
+  };
 }
