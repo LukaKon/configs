f87c563476714e471f2cacba7ddd19525f8f00d2
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-07-15 12:23:20 +0200

    mac gnome up

--- nix_mac/desktops/gnome.nix
+++ nix_mac/desktops/gnome.nix
@@ -9,39 +9,51 @@
 
     layout = "pl";
   };
-  environment.systemPackages = with pkgs;
-  [
-    gnome3.adwaita-icon-theme
-    gnome-breeze
-    gnomeExtensions.sound-output-device-chooser
-    gnomeExtensions.tilingnome
-    gnomeExtensions.paperwm
-    dconf2nix
-  ];
-  services.udev.packages = with pkgs;
-  [
-    gnome3.gnome-settings-daemon
-    gnome3.seahorse
-    gnome3.gnome-keyring
-    gnome3.gnome-desktop
-    gnome3.gnome-applets
-    gnome3.gnome-tweak-tool
-    gnome3.dconf-editor
-    gnome3.gnome-color-manager
-    gnome3.gnome-control-center
-  ];
-  environment.gnome.excludePackages = with pkgs;
-  [
-    gnome3.totem
-    gnome3.geary
-    gnome3.cheese
-    gnome3.gnome-maps
-    gnome3.simple-scan
-    gnome3.gnome-music
-    gnome3.gnome-contacts
-    gnome3.gnome-calendar
-    gnome3.gnome-terminal
-    gnome3.gnome-bluetooth
-    gnome3.gnome-system-monitor
-  ];
+
+    environment.systemPackages = with pkgs;
+    [
+      gnome3.adwaita-icon-theme
+      gnomeExtensions.sound-output-device-chooser
+      gnomeExtensions.gtile # Tile windows on a grid.
+      gnomeExtensions.windownavigator
+      gnomeExtensions.just-perfection
+      gnomeExtensions.tweaks-in-system-menu
+      gnomeExtensions.vitals
+      gnome.gnome-tweaks
+      gnome.gnome-notes # sticky notes
+      gnome-breeze
+
+
+      dconf2nix
+      brasero
+      # firefox-wayland
+      # foot
+    ];
+    services = {
+      udev.packages = with pkgs;
+      [
+        gnome3.gnome-settings-daemon
+        gnome3.seahorse
+        gnome3.gnome-keyring
+        gnome3.gnome-desktop
+        gnome3.gnome-applets
+        gnome3.gnome-tweak-tool
+      ];
+      dbus.packages = with pkgs;
+      [
+        gnome3.dconf
+      ];
+    };
+    environment.gnome.excludePackages = with pkgs;
+    [
+      gnome3.totem
+      gnome3.geary
+      gnome3.cheese
+      gnome3.gnome-maps
+      gnome3.simple-scan
+      gnome3.gnome-music
+      gnome3.gnome-contacts
+      gnome3.gnome-calendar
+      gnome.gnome-terminal
+    ];
 }
