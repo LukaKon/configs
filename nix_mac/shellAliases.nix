64bb5dfe300453235ef87cf2289f4679f6f7bdc3
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-07-23 08:40:53 +0200

    up

--- nix_mac/shellAliases.nix
+++ nix_mac/shellAliases.nix
@@ -1,41 +1,34 @@
 {config, lib, ...}:
 {
   environment.shellAliases = {
-                                # git
-                                gs = "git status -s";
-                                ga = "git add";
-                                gc = "git commit";
-                                gp = "git push";
-                                gpull = "git pull";
-                                gb = "git branch";
-                                gch = "git checkout";
-                                gm = "git merge";
-                                gf = "git fetch";
+    # git
+    gs = "git status -s";
+    ga = "git add";
+    gc = "git commit";
+    gp = "git push";
+    gpull = "git pull";
+    gb = "git branch";
+    gch = "git checkout";
+    gm = "git merge";
+    gf = "git fetch";
 
-                                # term
-                                susp = "systemctl suspend";
-                                reboot = "doas reboot";
+    # term
+    susp = "systemctl suspend";
+    reboot = "doas reboot";
 
-                                # editor
-                                vi = "nvim";
+    # editor
+    vi = "nvim";
 
-                                # nix
-                                # upnix = "sudo nix-channel --update && nix-env -u";
-                                rebuild = "sudo nixos-rebuild switch";
-                                upos = "sudo nixos-rebuild switch --upgrade";
+    # nix
+    rebuild = "sudo nixos-rebuild switch";
+    upos = "sudo nixos-rebuild switch --upgrade";
 
-                                # btrfs snapshots
-                                # sb = "python ${path_script}snapshot.py";
-                                sb = "python3 /home/lk/configs/scripts/snapshot.py";
+    # btrfs snapshots
+    sb = "python3 /home/lk/configs/scripts/snapshot_mac.py";
 
-                                # backup config files to git
-                                cong = "python3 /home/lk/configs/scripts/git_conf.py";
-                                # deploy nix config files in /etc/nixos and rebuild system
-                                nixdep = "python3 /home/lk/configs/scripts/nix_deploy_mac.py";
-
-                                # rsync
-                                # data
-                                #rdata = "cd && rsync -rav --exclude=".snapshots" data/ ssh
-                                #lk@192.168.0.2:/volume2/homes/lk/Drive/pliki/";
-                              };
-                            }
+    # backup config files to git
+    cong = "python3 /home/lk/configs/scripts/git_conf.py";
+    # deploy nix config files in /etc/nixos and rebuild system
+    nixdep = "python3 /home/lk/configs/scripts/nix_deploy_mac.py";
+  };
+}
