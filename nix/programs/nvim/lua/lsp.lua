9aceea7b1cce0ea7c06c8e773459b2b97146eabb
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-07-25 18:24:30 +0200

    up

--- nix/programs/nvim/lua/lsp.lua
+++ nix/programs/nvim/lua/lsp.lua
@@ -26,7 +26,8 @@ require'compe'.setup {
   max_abbr_width = 100;
   max_kind_width = 100;
   max_menu_width = 100;
-  documentation = false;
+  documentation = true;
+  -- documentation = false;
 
   source = {
     path = true;
