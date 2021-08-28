ed27c4e40c95331357fcc7da6e74a816a247146e
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-08-28 10:16:57 +0200

    up

--- nix_rasp/desktops/qtile.nix
+++ nix_rasp/desktops/qtile.nix
@@ -18,20 +18,20 @@
 		overlays = [
 			(self: super: {
 				qtile = super.qtile.overrideAttrs(oldAttrs: {
-				pythonPath = oldAttrs.pythonPath ++ (with self.python39Packages; [
-				keyring
-				xcffib
-				#cairocffi-xcffib
-				setuptools
-				setuptools_scm
-				dateutil
-				dbus-python
-				mpd2
-				psutil
-				pyxdg
-				pygobject3
-			]);
-		});
-	})];
-};
+					pythonPath = oldAttrs.pythonPath ++ (with self.python39Packages; [
+					keyring
+					xcffib
+					#cairocffi-xcffib
+					setuptools
+					setuptools_scm
+					dateutil
+					dbus-python
+					mpd2
+					psutil
+					pyxdg
+					pygobject3
+				]);
+			});
+		})];
+	};
 }
