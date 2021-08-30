9ed9d802f5820bcb5bfd0b9ee91555161bc7dd24
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-08-30 19:36:32 +0200

    up

--- nix_mac/configuration.nix
+++ nix_mac/configuration.nix
@@ -4,24 +4,24 @@
 	imports =
 		[ # Include the results of the hardware scan.
 		./hardware-configuration.nix
-		../nix/system.nix		# Base system settings
-		../nix/btrfs.nix
+		./system.nix		# Base system settings
+		./btrfs.nix
 		./network.nix		# Network configuration
-		../nix/programs.nix		# List of installed programs
-		../nix/security/doas.nix	# Add 'doas'
-		../nix/services/openssh.nix	# SSH
-		../nix/programs/postgresql.nix	# PostgreSQL
+		./programs.nix		# List of installed programs
+		./security/doas.nix	# Add 'doas'
+		./services/openssh.nix	# SSH
+		./programs/postgresql.nix	# PostgreSQL
 		# ./desktops/kde.nix
-		../nix/desktops/gnome.nix	# Gnome window manager settings
+		./desktops/gnome.nix	# Gnome window manager settings
 		# ./services/keyring.nix	# included in Gnome desktop
 		./services/laptop.nix   # settings for laptop
 		# ./services/remote.nix
-		../nix/programs/zsh.nix	# zsh and console settings
-		../nix/fonts.nix		# Fonts to install
-		../nix/users.nix		# Users configurations
-		../nix/shellAliases.nix	# Shell aliases
-		../nix/virtualisation/docker.nix	# Docker
+		./programs/zsh.nix	# zsh and console settings
+		./fonts.nix		# Fonts to install
+		./users.nix		# Users configurations
+		./shellAliases.nix	# Shell aliases
+		./virtualisation/docker.nix	# Docker
 		./programs/nvim/nvim.nix
-		../nix/services/flatpak.nix
+		./services/flatpak.nix
 	];
 }
