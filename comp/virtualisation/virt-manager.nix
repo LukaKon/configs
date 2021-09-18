89374e2cb45e8c3ce438e4f9499108623a77eeff
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-09-18 18:07:33 +0200

    up

--- comp/virtualisation/virt-manager.nix
+++
@@ -1,8 +0,0 @@
-{pkgs, ... }:
-
-{
-    boot.kernelModules = [ "kvm-intel" ];
-    virtualisation.libvirtd.enable = true;
-    programs.dconf.enable = true;
-    environment.systemPackages = with pkgs; [ virt-manager ];
-}
