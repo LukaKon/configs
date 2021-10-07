e115d13d6aa8e91cbd6a62cf160270f47c9ab177
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-10-07 20:51:28 +0200

    nvim mod

--- comp/fuji.nix
+++ comp/fuji.nix
@@ -16,7 +16,7 @@
     # ./../modules/system/cron.nix
     ./../modules/system/shellAliases.nix  # Shell aliases
     ./../modules/system/users.nix     # Users configurations
-    ./../modules/system/hdd.nix           # Additional HDD
+    # ./../modules/system/hdd.nix           # Additional HDD
 
     # Security
     ./../modules/security/doas.nix # Add 'doas'
--- modules/system/hdd.nix
+++ modules/system/hdd.nix
@@ -2,14 +2,14 @@
 
 {
   fileSystems."/home/lk/data" = {
-                      device = "/dev/disk/by-uuid/71bff5ad-3209-4d8f-8ead-65733cfb4b88";
-                      fsType = "btrfs";
-                      options = ["subvol=@data"];
-                  };
+    device = "/dev/disk/by-uuid/71bff5ad-3209-4d8f-8ead-65733cfb4b88";
+    fsType = "btrfs";
+    options = ["subvol=@data"];
+    };
 
-  fileSystems."/home/lk/vm" = {
-                      device = "/dev/disk/by-uuid/ef94fe71-c21b-4da8-aabb-e80588efc941";
-                      fsType = "btrfs";
-                      options = ["subvol=@vm"];
-                  };
+  # fileSystems."/home/lk/vm" = {
+  #                     device = "/dev/disk/by-uuid/ef94fe71-c21b-4da8-aabb-e80588efc941";
+  #                     fsType = "btrfs";
+  #                     options = ["subvol=@vm"];
+  #                 };
 }
