11039b32859688704dc7448e3dd7c95cfbce7bae
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-01-21 11:50:38 +0100

    add remote and kk user

--- modules/services/remote.nix
+++ modules/services/remote.nix
@@ -7,5 +7,7 @@
     services.xrdp.enable = true;
     # services.xrdp.defaultWindowManager = "${pkgs.openbox}/bin/openbox";
     # services.xrdp.defaultWindowManager = "${pkgs.icewm}/bin/icewm";
+    services.xrdp.defaultWindowManager = "startplasma-x11";
+
     networking.firewall.allowedTCPPorts = [ 3389 ];
 }
