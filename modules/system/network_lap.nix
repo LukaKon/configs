c7eb64e632c85ea808002ec6f49668ca11bff0fa
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-02-01 17:50:07 +0100

    mac -> lap

--- modules/system/network_lap.nix
+++ modules/system/network_lap.nix
@@ -14,21 +14,21 @@
 	# programs.mtr.enable = true;
 
 	networking = {
-		hostName = "mac"; # Define your hostname.
-		domain = "luka.mac.org";
+		hostName = "lap"; # Define your hostname.
+		domain = "luka.lap.org";
 					# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 					networkmanager.enable = true;
 
-					# interfaces.wlp1s0.ipv4.addresses = [{
-					#			address = "192.168.0.3";
-					#			prefixLength = 24;
-					#		}];
-					# interfaces.enp0s31f6.ipv4.addresses = [{
-					#			address = "192.168.1.3";
-					#			prefixLength = 24;
-					#		}];
+					interfaces.wlp1s0.ipv4.addresses = [{
+							address = "192.168.0.4";
+							prefixLength = 24;
+						}];
+					interfaces.enp0s31f6.ipv4.addresses = [{
+							address = "192.168.1.4";
+							prefixLength = 24;
+						}];
 
 					defaultGateway = "192.168.0.1";
-					nameservers = ["37.8.214.2" "31.11.202.254"];
+					#nameservers = ["37.8.214.2" "31.11.202.254"];
 				};
 			}
