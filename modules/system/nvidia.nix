6db9c116dc9846844e13e9dab5663beea389aeec
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-03-14 21:13:14 +0100

    nvidia rm nvtop -> generte error

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -5,7 +5,7 @@
   # services.xserver.videoDrivers = ["modesetting" "nvidia"];
   environment.systemPackages = with pkgs;
   [
-    nvtop
-    # cudaPackages.cudatoolkit_11
+    #nvtop
+    cudaPackages.cudatoolkit_11
   ];
 }
