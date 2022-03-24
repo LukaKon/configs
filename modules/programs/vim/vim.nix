c0d371a80c92e0a9fcb8a540029743bc0babd8f3
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-03-24 20:35:39 +0100

    nvim-

---
+++ modules/programs/vim/vim.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+{
+  environment.variables = { EDITOR = "vim"; };
+
+
+  environment.systemPackages = with pkgs; [
+    ((vim_configurable.override { python = python3; }).customize{
+      name = "vim";
+      vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
+        start = [ vim-nix vim-lastplace ];
+        opt = [];
+      };
+      vimrcConfig.customRC = ''
+        " your custom vimrc
+        set nocompatible
+        set backspace=indent,eol,start
+        " ...
+      '';
+    }
+  )];
+}
