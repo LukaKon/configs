4ae3ec080997f0f0a324f6b25212bb3673db20e0
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-04-20 22:19:24 +0200

    up sys variables: nvim

--- modules/system/env_variables.nix
+++ modules/system/env_variables.nix
@@ -4,5 +4,8 @@
     environment.variables = {
         EMAIL_USER = "lukasz.konieczny.lk@gmail.com";
         EMAIL_PASS = "omxnszhnclgymoqy";
+
+        EDITOR = "nvim";
+        VISUAL = "nvim";
     };
 }
