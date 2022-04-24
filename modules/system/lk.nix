778eb2433b1711ed1957a78c7827731fd3693d1f
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-04-24 11:22:57 +0200

    change python to 3.10

--- modules/system/lk.nix
+++ modules/system/lk.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ...}:# neovim-flake, ... }:
+{ config, pkgs, ... }: # neovim-flake, ... }:
 
 {
   users.users.lk = {
@@ -8,84 +8,84 @@
     extraGroups = [ "wheel" "networkmanager" "dialout" "libvirtd" "docker" "video" ];
     shell = pkgs.zsh;
     packages = with pkgs;
-    [
-      dotnet-sdk
-      clang
-      clang-tools
-      #gcc
-      cmake
-      #bmake
-      # luajit
-      nodejs
-      # deno
-
-      # wezterm
-      #kitty
-
-      #firejail
-
-      # qmk
-      qmk
-      qmk-udev-rules
-
-      # www
-      # qutebrowser
-      ungoogled-chromium
-
-      # IDE
-      # neovim
-
-      # neovim-flake.defaultPackage.x86_64-linux
-
-      helix
-      nodePackages.vscode-langservers-extracted
-      nodePackages.dockerfile-language-server-nodejs
-      nodePackages.typescript-language-server
-
-      rnix-lsp
-
-      # spacevim
-      # neovide
-      # uivonim
-      # vscode
-      vscodium
-      # eclipses.eclipse-jee
-      arduino
-      # jetbrains.webstorm
-      # jetbrains.pycharm-community
-      jetbrains.pycharm-professional
-      # thonny
-      # unityhub
-      dbeaver
-      # postman
-
-      # comunicator
-      # signal-desktop
-      slack
-      discord
-
-      # video conference
-      zoom-us
-
-      # LaTeX
-      texlive.combined.scheme-full
-      # texstudio
-      # mindforger
-
-      (python39.withPackages(ps: with ps;
       [
-        python-lsp-server
-        # autopep8
-        # flake8
-        # colorama
-        psutil
-        pip
-        pynvim
-        yapf
-        jedi
-        # pylint
-        # jupyter
-      ]))
-    ];
+        dotnet-sdk
+        clang
+        clang-tools
+        #gcc
+        cmake
+        #bmake
+        # luajit
+        nodejs
+        # deno
+
+        # wezterm
+        #kitty
+
+        #firejail
+
+        # qmk
+        qmk
+        qmk-udev-rules
+
+        # www
+        # qutebrowser
+        ungoogled-chromium
+
+        # IDE
+        # neovim
+
+        # neovim-flake.defaultPackage.x86_64-linux
+
+        helix
+        nodePackages.vscode-langservers-extracted
+        nodePackages.dockerfile-language-server-nodejs
+        nodePackages.typescript-language-server
+
+        rnix-lsp
+
+        # spacevim
+        # neovide
+        # uivonim
+        # vscode
+        vscodium
+        # eclipses.eclipse-jee
+        arduino
+        # jetbrains.webstorm
+        # jetbrains.pycharm-community
+        jetbrains.pycharm-professional
+        # thonny
+        # unityhub
+        dbeaver
+        # postman
+
+        # comunicator
+        # signal-desktop
+        slack
+        discord
+
+        # video conference
+        zoom-us
+
+        # LaTeX
+        texlive.combined.scheme-full
+        # texstudio
+        # mindforger
+
+        (python310.withPackages (ps: with ps;
+        [
+          python-lsp-server
+          # autopep8
+          # flake8
+          # colorama
+          psutil
+          pip
+          pynvim
+          yapf
+          jedi
+          # pylint
+          # jupyter
+        ]))
+      ];
   };
 }
