1548a3f33a0a394d05ce282e737943e14628c84b
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-04-30 22:50:56 +0200

    qtile conf: playing with widgets

--- modules/desktops/qtile.nix
+++ modules/desktops/qtile.nix
@@ -1,39 +1,18 @@
-{config, lib, ...}:
+{ config, lib, ... }:
 
 {
-	services.xserver = {
-		enable = true;
-		# videoDrivers = ["modesetting" "nvidia"];
-		# displayManager.startx.enable = true;  # necessary to
-		# create .xinitrc file
+  services.xserver = {
+    enable = true;
+    # videoDrivers = ["modesetting" "nvidia"];
+    # displayManager.startx.enable = true;  # necessary to
+    # create .xinitrc file
 
-		# Qtile
-		displayManager.lightdm.enable = true;
-		windowManager.qtile.enable = true;
-		displayManager.defaultSession = "none+qtile";
+    # Qtile
+    #displayManager.lightdm.enable = true;
+    displayManager.gdm.wayland = true;
+    windowManager.qtile.enable = true;
+    displayManager.defaultSession = "none+qtile";
 
-		layout = "pl";
-	};
-
-	#nixpkgs = {
-	#	overlays = [
-	#		(self: super: {
-	#			qtile = super.qtile.overrideAttrs(oldAttrs: {
-	#				pythonPath = oldAttrs.pythonPath ++ (with self.python39Packages;
-	#				[
-	#					keyring
-	#					xcffib
-	#					#cairocffi-xcffib
-	#					setuptools
-	#					setuptools_scm
-	#					dateutil
-	#					dbus-python
-	#					mpd2
-	#					psutil
-	#					pyxdg
-	#					pygobject3
-	#				]);
-	#			});
-	#		})];
-	#	};
+    layout = "pl";
+  };
 }
