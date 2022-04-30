9a40095f0e559916056241623ff8d92b11043032
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-04-30 22:50:56 +0200

    qtile conf: playing with widgets

--- conf_files/qtile/config.py
+++ conf_files/qtile/config.py
@@ -35,12 +35,12 @@ from libqtile.dgroups import simple_key_binder
 mod = "mod4"
 terminal = guess_terminal()
 # browser='qutebrowser'
-browser='firefox'
+browser = "firefox"
 
-FONT='fira'
-SF=10
-MF=11
-LF=12
+FONT = "fira"
+SF = 10
+MF = 11
+LF = 12
 
 # colors
 GREY = "#d1d1d1"
@@ -50,7 +50,7 @@ BLUE = "#6ea1f8"
 DARK_BLUE = "#2e0fce"
 ORANGE = "#ff9235"
 DARK_ORANGE = "#d16608"
-LIGHT_GREEN='#8feecc'
+LIGHT_GREEN = "#8feecc"
 GREEN = "#31ce43"
 DARK_GREEN = "#4b8734"
 RED = "#ff4f4f"
@@ -60,69 +60,66 @@ BLACK = "#000000"
 keys = [
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-
     # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(),
-        desc="Move window focus to other window"),
-
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
-        desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
-        desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
-        desc="Move window down"),
+    Key(
+        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
+    ),
+    Key(
+        [mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right",
+    ),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(),
-        desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(),
-        desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(),
-        desc="Grow window down"),
+    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key(
+        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
+    ),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack"),
-
+    Key(
+        [mod, "shift"],
+        "Return",
+        lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack",
+    ),
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(),
-        desc="Spawn a command using a prompt widget"),
-
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
     # programs
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    Key([mod], 'b', lazy.spawn(browser), desc='Launch web browser'),
-    Key([mod], 'g', lazy.spawn('chromium'), desc='Launch chromium'),
-    Key([mod], 'c', lazy.spawn('codium'), desc='Launch vscodium'),
-    Key([mod], 'd', lazy.spawn('dbeaver'), desc='Launch dbeaver'),
-    Key([mod], 'v', lazy.spawn('virt-manager'), desc='Launch virt-manager'),
-    Key([mod], 'y', lazy.spawn('freetube'), desc='Launch free tube'),
-    Key([mod], 'z', lazy.spawn('slack'), desc='Launch slack'),
-    Key([mod], 'f', lazy.spawn('flameshot'), desc='Launch flameshot'),
-
+    Key([mod], "b", lazy.spawn(browser), desc="Launch web browser"),
+    Key([mod], "g", lazy.spawn("chromium"), desc="Launch chromium"),
+    Key([mod], "c", lazy.spawn("codium"), desc="Launch vscodium"),
+    Key([mod], "d", lazy.spawn("dbeaver"), desc="Launch dbeaver"),
+    Key([mod], "v", lazy.spawn("virt-manager"), desc="Launch virt-manager"),
+    Key([mod], "y", lazy.spawn("freetube"), desc="Launch free tube"),
+    Key([mod], "z", lazy.spawn("slack"), desc="Launch slack"),
+    Key([mod], "f", lazy.spawn("flameshot"), desc="Launch flameshot"),
     # Key([], "XF86AudioMute", lazy.spawn("amixer -q set Master toggle")),
     # Key([], "XF86AudioLowerVolume", lazy.spawn("amixer -c 0 sset Master 1- unmute")),
     # Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer -c 0 sset Master 1+ unmute")),
 ]
 
-#groups = [
+# groups = [
 #      Group("term", layout='monadwide'),
 #      Group("dev", layout='monadtall'),
 #   Group("www", layout='monadtall'),
@@ -135,16 +132,16 @@ keys = [
 #   Group("float", layout='floating'),
 # ]
 groups = [
-    Group("1", layout='Bsp'),
-    Group("2", layout='monadtall'),
-    Group("3", layout='monadtall'),
-    Group("4", layout='monadtall'),
-    Group("5", layout='monadtall'),
-    Group("6", layout='monadtall'),
-    Group("7", layout='monadtall'),
-    Group("8", layout='monadtall'),
-    Group("9", layout='monadtall'),
-    Group("0", layout='floating'),
+    Group("1", layout="Bsp"),
+    Group("2", layout="monadtall"),
+    Group("3", layout="monadtall"),
+    Group("4", layout="monadtall"),
+    Group("5", layout="monadtall"),
+    Group("6", layout="monadtall"),
+    Group("7", layout="monadtall"),
+    Group("8", layout="monadtall"),
+    Group("9", layout="monadtall"),
+    Group("0", layout="floating"),
 ]
 
 # groups = [Group(i,layout='monadtall') for i in "1234567890"]
@@ -152,19 +149,28 @@ groups = [
 # dgroups_key_binder = simple_key_binder(mod)
 
 for gr in groups:
-    keys.extend([
-        # mod1 + letter of group = switch to group
-        Key([mod], gr.name, lazy.group[gr.name].toscreen(),
-            desc=f"Switch to group {gr.name}"),
-
-        # mod1 + shift + letter of group = switch to & move focused window to group
-        Key([mod, "shift"], gr.name, lazy.window.togroup(gr.name, switch_group=True),
-            desc=f"Switch to & move focused window to group {gr.name}"),
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-        #     desc="move focused window to group {}".format(i.name)),
-])
+    keys.extend(
+        [
+            # mod1 + letter of group = switch to group
+            Key(
+                [mod],
+                gr.name,
+                lazy.group[gr.name].toscreen(),
+                desc=f"Switch to group {gr.name}",
+            ),
+            # mod1 + shift + letter of group = switch to & move focused window to group
+            Key(
+                [mod, "shift"],
+                gr.name,
+                lazy.window.togroup(gr.name, switch_group=True),
+                desc=f"Switch to & move focused window to group {gr.name}",
+            ),
+            # Or, use below if you prefer not to switch to that group.
+            # # mod1 + shift + letter of group = move focused window to group
+            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+            #     desc="move focused window to group {}".format(i.name)),
+        ]
+    )
 
 layout_theme = {
     "border_width": 2,
@@ -174,18 +180,15 @@ layout_theme = {
 }
 
 layouts = [
-    layout.Columns(
-        border_focus_stack=[WHITE, LIGHT_GREEN],
-        border_width=2
-        ),
+    layout.Columns(border_focus_stack=[WHITE, LIGHT_GREEN], border_width=2),
     layout.Bsp(
         border_focus=LIGHT_BLUE,
         border_normal=GREY,
         border_on_single=False,
         border_width=2,
         grow_ammount=5,
-        ratio=1.6
-        ),
+        ratio=1.6,
+    ),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
@@ -200,13 +203,9 @@ layouts = [
     # layout.Zoomy(),
 ]
 
-widget_defaults = dict(
-    font=FONT,
-    fontsize=MF,
-    padding=3,
-    background=DARK_GREY
-)
+widget_defaults = dict(font=FONT, fontsize=MF, padding=3, background=DARK_GREY)
 extension_defaults = widget_defaults.copy()
+
 
 def init_widgets_list():
     widgets_list = [
@@ -215,6 +214,7 @@ def init_widgets_list():
             format="CPU {freq_current}GHz  {load_percent}%",
         )
     ]
+
 
 screens = [
     Screen(
@@ -227,14 +227,18 @@ screens = [
                 widget.WindowName(),
                 widget.Chord(
                     chords_colors={
-                        'launch': ("#ff0000", "#ffffff"),
+                        "launch": ("#ff0000", "#ffffff"),
                     },
                     name_transform=lambda name: name.upper(),
+                ),
+                widget.CPU(),
+                widget.Memory(
+                    measure_mem="G",
                 ),
                 # widget.TextBox("default config", name="default"),
                 # widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
                 widget.Systray(),
-                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
                 widget.QuickExit(),
             ],
             24,
@@ -246,11 +250,16 @@ screens = [
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
-         start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
-         start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+    Drag(
+        [mod],
+        "Button1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position(),
+    ),
+    Drag(
+        [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
+    ),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
 dgroups_key_binder = None
@@ -258,16 +267,18 @@ dgroups_app_rules = []  # type: List
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
-floating_layout = layout.Floating(float_rules=[
-    # Run the utility of `xprop` to see the wm class and name of an X client.
-    *layout.Floating.default_float_rules,
-    Match(wm_class='confirmreset'),  # gitk
-    Match(wm_class='makebranch'),  # gitk
-    Match(wm_class='maketag'),  # gitk
-    Match(wm_class='ssh-askpass'),  # ssh-askpass
-    Match(title='branchdialog'),  # gitk
-    Match(title='pinentry'),  # GPG key password entry
-])
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
