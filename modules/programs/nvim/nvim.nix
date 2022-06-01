a5852ad6e917c08a5a3c134f93868de5032917fc
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-06-01 07:46:42 +0200

    up

--- modules/programs/nvim/nvim.nix
+++ modules/programs/nvim/nvim.nix
@@ -1,15 +1,8 @@
-{pkgs, config, ...}:
+{ pkgs, config, ... }:
 
 {
-  # nixpkgs.overlays = [
-  #   (import (builtins.fetchTarball {
-  #     url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
-  #     sha256 = "01dkd7w7q7klqfiszfrx9jbq2n149500hcs0799bi8b6zg0q56hn";
-  #   }))
-  # ];
-
   environment = {
-    variables = {EDITOR = "nvim"; VISUAL = "nvim";};
+    variables = { EDITOR = "nvim"; VISUAL = "nvim"; };
     systemPackages = with pkgs;[
       ripgrep
       ctags
@@ -33,138 +26,137 @@
                   tree-sitter-javascript
                   tree-sitter-css
                 ]
-                ))
+              ))
 
-                # File tree
-                nvim-tree-lua
+              # File tree
+              nvim-tree-lua
 
-                # Eyecandy
-                # nvim-treesitter
-                # bufferline-nvim
-                galaxyline-nvim
-                nvim-colorizer-lua
+              # Eyecandy
+              # nvim-treesitter
+              # bufferline-nvim
+              galaxyline-nvim
+              nvim-colorizer-lua
 
-                # dusk-vim
-                pears-nvim
+              # dusk-vim
+              pears-nvim
 
-                # Telescope
-                telescope-nvim
+              # Telescope
+              telescope-nvim
 
-                # Indent lines
-                indent-blankline-nvim
+              # Indent lines
+              indent-blankline-nvim
 
-                # file explorer
-                The_NERD_tree
-                # nerdtree
-                nerdtree-git-plugin
-                vim-devicons
-                vim-nerdtree-syntax-highlight
-                vim-nerdtree-tabs
+              # file explorer
+              The_NERD_tree
+              # nerdtree
+              nerdtree-git-plugin
+              vim-devicons
+              vim-nerdtree-syntax-highlight
+              vim-nerdtree-tabs
 
-                fzf-vim
-                # python-mode # TODO: was switched off
-                vim-multiple-cursors
-                vim-commentary
-                splitjoin-vim     # gS - split; gJ - join
-                ale
-                vim-mundo
+              fzf-vim
+              # python-mode # TODO: was switched off
+              vim-multiple-cursors
+              vim-commentary
+              splitjoin-vim # gS - split; gJ - join
+              ale
+              vim-mundo
 
-                vim-autoformat
+              vim-autoformat
 
-                # nvim-lspconfig
-                completion-nvim
+              # nvim-lspconfig
 
-                vimspector
+              vimspector
 
-                tabular
-                syntastic
-                vim-snippets
-                vim-nix
-                vim-python-pep8-indent
-                vim-csharp
-                neomake
-                neoformat
-                # SimpylFold
-                # neoterm
-                vim-javascript
-                vim-javascript-syntax
-                emmet-vim
-                coc-fzf
-                coc-emmet
-                coc-pyright
-                coc-html
-                coc-nvim
-                coc-yank
-                coc-css
-                coc-texlab
-                coc-lua
-                coc-stylelint
-                coc-json
-                coc-smartf
-                coc-lists
-                coc-cmake
-                coc-pairs
-                coc-prettier
-                coc-spell-checker
-                coc-highlight
+              tabular
+              syntastic
+              vim-snippets
+              vim-nix
+              vim-python-pep8-indent
+              vim-csharp
+              neomake
+              neoformat
+              # SimpylFold
+              # neoterm
+              vim-javascript
+              vim-javascript-syntax
+              emmet-vim
+              coc-fzf
+              coc-emmet
+              coc-pyright
+              coc-html
+              coc-nvim
+              coc-yank
+              coc-css
+              coc-texlab
+              coc-lua
+              coc-stylelint
+              coc-json
+              coc-smartf
+              coc-lists
+              coc-cmake
+              coc-pairs
+              coc-prettier
+              coc-spell-checker
+              coc-highlight
 
-                vim-speeddating # Change dates fast
+              vim-speeddating # Change dates fast
 
-                vim-repeat # Repeat stuff
+              vim-repeat # Repeat stuff
 
-                quick-scope # Text Navigation
-                vim-illuminate # highlight all matches under cursor
+              quick-scope # Text Navigation
+              vim-illuminate # highlight all matches under cursor
 
-                vim-polyglot # Better syntax support
+              vim-polyglot # Better syntax support
 
-                auto-pairs # Auto pairs for '(' '[' '{'
+              auto-pairs # Auto pairs for '(' '[' '{'
 
-                rainbow # Rainbow brackets
+              rainbow # Rainbow brackets
 
-                # Start screen
-                vim-startify
+              # Start screen
+              vim-startify
 
-                vim-jinja
-                vim-yapf
-                emmet-vim
-                jedi-vim
+              vim-jinja
+              vim-yapf
+              emmet-vim
+              jedi-vim
 
-                nginx-vim
+              nginx-vim
 
-                unicode-vim
-                tagbar
+              unicode-vim
+              tagbar
 
-                colorizer
-                vim-surround
+              colorizer
+              vim-surround
 
-                deoplete-zsh
-                deoplete-jedi
-                deoplete-nvim
-                deoplete-github
+              deoplete-zsh
+              deoplete-jedi
+              deoplete-nvim
+              deoplete-github
 
-                # git
-                vim-gitgutter
-                vim-gitbranch
-                vim-signify
-                vim-fugitive
-                vim-rhubarb
-                gv-vim
+              # git
+              vim-gitgutter
+              vim-gitbranch
+              vim-signify
+              vim-fugitive
+              vim-rhubarb
+              gv-vim
 
-                popup-nvim
-                nvim-compe
-                plenary-nvim
+              popup-nvim
+              nvim-compe
+              plenary-nvim
 
-                gruvbox-community
-                vim-airline-themes
-                vim-airline
-              ];
-              opt = [
-              ];
-            };
-            customRC = builtins.readFile ./nvim.vim; #./boot.vim; ./init.vim;
+              gruvbox-community
+              vim-airline-themes
+              vim-airline
+            ];
+            opt = [
+            ];
           };
-        }
-        )
-      ];
-    };
-  }
+          customRC = builtins.readFile ./nvim.vim; #./boot.vim; ./init.vim;
+        };
+      }
+      )
+    ];
+  };
+}
