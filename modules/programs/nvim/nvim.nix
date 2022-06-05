60dd6ce95eaa62225dc3dfe95a02ada5ccc20c6e
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-06-05 21:51:45 +0200

    nvim: lua not working...

--- modules/programs/nvim/nvim.nix
+++ modules/programs/nvim/nvim.nix
@@ -12,16 +12,51 @@
         configure = {
           packages.myPlugins = with pkgs.vimPlugins; {
             start = [
-         
-
+              packer-nvim
+              popup-nvim
+              plenary-nvim
+              nvim-whichkey-setup-lua
+              nvim-autopairs
+              nvim-web-devicons
               # File tree
               nvim-tree-lua
 
+              bufferline-nvim
+              vim-bbye
+              lualine-nvim
+              toggleterm-nvim
+              impatient-nvim
+              indent-blankline-nvim
+              FixCursorHold-nvim
 
+              # cmp plugins
+             nvim-cmp
+             cmp-buffer
+             cmp-path
+             cmp-cmdline
+             cmp_luasnip
+             cmp-nvim-lsp
 
+              # snippets
+              luasnip
+              friendly-snippets
+
+              # LSP
+              nvim-lspconfig
+              null-ls-nvim
+
+              # Treesitter
+              nvim-treesitter
+
+             nvim-comment
+             # git
+             gitsigns-nvim
+             
+             
              
 
-              gruvbox-community
+              #gruvbox-community
+              gruvbox
           
             ];
             opt = [
