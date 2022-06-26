b2af6cad372d2d629f265925c1a5351e6373fa89
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-06-26 12:58:02 +0200

    up

--- modules/services/openssh.nix
+++ modules/services/openssh.nix
@@ -3,7 +3,7 @@
 {
     services.openssh = {
         enable = true;
-        passwordAuthentication = false;
+        #passwordAuthentication = false;
     };
         # users.users.lk.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBSpljbG2//f7ln7RMOuV++QOUdzr7duIVLXhE2zpahE lukasz.konieczny.lk@protonmail.com" ];
 }
