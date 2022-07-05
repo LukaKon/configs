19d90693c408a88e5819cd110e4a86e1cf9b4bfa
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-07-05 20:33:40 +0200

    up

--- modules/desktops/qtile/default.nix
+++ modules/desktops/qtile/default.nix
@@ -3,13 +3,9 @@
 {
   services.xserver = {
     enable = true;
-    # videoDrivers = ["modesetting" "nvidia"];
-    # displayManager.startx.enable = true;  # necessary to
-    # create .xinitrc file
 
     # Qtile
     displayManager.lightdm.enable = true;
-    # displayManager.gdm.wayland.enable = true;
     windowManager.qtile.enable = true;
     displayManager.defaultSession = "none+qtile";
 
