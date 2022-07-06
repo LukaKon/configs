60f06a100bde74f911c5082e43457effa15e0809
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-07-06 07:58:13 +0200

    lap add keyring

--- modules/services/keyring.nix
+++ modules/services/keyring.nix
@@ -1,13 +1,13 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 # keyring for non GNOME desktop
 {
   environment = {
     systemPackages = with pkgs;
-    [
-      gnome3.gnome-keyring
-      gnome3.seahorse
-    ];
+      [
+        gnome3.gnome-keyring
+        gnome3.seahorse
+      ];
   };
 
   services.gnome.gnome-keyring.enable = true;
@@ -15,7 +15,7 @@
 
   programs = {
     seahorse.enable = true; # for gnome-keyring gui application
-    dconf.enable = true;  # for gnome applications
-    gnupg.agent = {enable = true; enableSSHSupport = true;};
+    dconf.enable = true; # for gnome applications
+    gnupg.agent = { enable = true; enableSSHSupport = true; };
   };
 }
