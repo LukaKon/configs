c8dca409e3fbce8056d75a2f65c6ea11f9470257
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-07-08 12:10:00 +0200

    nvim again...

--- modules/programs/nvim/nvim.nix
+++ modules/programs/nvim/nvim.nix
@@ -1,68 +1,95 @@
 { pkgs, config, ... }:
 
 {
-  environment = {
-    variables = { EDITOR = "nvim"; VISUAL = "nvim"; };
-    systemPackages = with pkgs;[
-      # ripgrep
-      # ctags
-      # gcc
+  programs.neovim = {
+    enable = true;
+    package = pkgs.neovim-unwrapped;
+    viAlias = true;
+    defaultEditor = true;
+    withPython3 = true;
+    withNodeJs = true;
+    configure = {
+      customRC = builtins.readFile ./init.vim;
+      packages.myPlugins = with pkgs.vimPlugins;{
+        start = [
+          packer-nvim
+          popup-nvim
+          plenary-nvim
+          nvim-whichkey-setup-lua
+          nvim-autopairs
+          nvim-web-devicons
+          # File tree
+          nvim-tree-lua
 
-      (neovim.override {
-        configure = {
-          packages.myPlugins = with pkgs.vimPlugins; {
-            start = [
-              packer-nvim
-              popup-nvim
-              plenary-nvim
-              nvim-whichkey-setup-lua
-              nvim-autopairs
-              nvim-web-devicons
-              # File tree
-              nvim-tree-lua
-
-              bufferline-nvim
-              vim-bbye
-              lualine-nvim
-              toggleterm-nvim
-              impatient-nvim
-              indent-blankline-nvim
-              FixCursorHold-nvim
-
-              # cmp plugins
-              nvim-cmp
-              cmp-buffer
-              cmp-path
-              cmp-cmdline
-              cmp_luasnip
-              cmp-nvim-lsp
-
-              # snippets
-              luasnip
-              friendly-snippets
-
-              # LSP
-              nvim-lspconfig
-              null-ls-nvim
-
-              # Treesitter
-              nvim-treesitter
-
-              nvim-comment
-              # git
-              gitsigns-nvim
-
-              #gruvbox-community
-              gruvbox
-
-            ];
-            opt = [
-            ];
-          };
-          customRC = builtins.readFile ./init.lua; #./nvim.vim;
-        };
-      }
-      )
-    ];
+          bufferline-nvim
+        ];
+        opt = [ ];
+      };
+    };
   };
+
+  #   environment = {
+  #     variables = { EDITOR = "nvim"; VISUAL = "nvim"; };
+  #     systemPackages = with pkgs;[
+  #       # ripgrep
+  #       # ctags
+  #       # gcc
+
+  #       (neovim.override {
+  #         configure = {
+  #           packages.myPlugins = with pkgs.vimPlugins; {
+  #             start = [
+  #               packer-nvim
+  #               popup-nvim
+  #               plenary-nvim
+  #               nvim-whichkey-setup-lua
+  #               nvim-autopairs
+  #               nvim-web-devicons
+  #               # File tree
+  #               nvim-tree-lua
+
+  #               bufferline-nvim
+  #               vim-bbye
+  #               lualine-nvim
+  #               toggleterm-nvim
+  #               impatient-nvim
+  #               indent-blankline-nvim
+  #               FixCursorHold-nvim
+
+  #               # cmp plugins
+  #               nvim-cmp
+  #               cmp-buffer
+  #               cmp-path
+  #               cmp-cmdline
+  #               cmp_luasnip
+  #               cmp-nvim-lsp
+
+  #               # snippets
+  #               luasnip
+  #               friendly-snippets
+
+  #               # LSP
+  #               nvim-lspconfig
+  #               null-ls-nvim
+
+  #               # Treesitter
+  #               nvim-treesitter
+
+  #               nvim-comment
+  #               # git
+  #               gitsigns-nvim
+
+  #               #gruvbox-community
+  #               gruvbox
+
+  #             ];
+  #             opt = [
+  #             ];
+  #           };
+  #           customRC = builtins.readFile ./init.lua; #./nvim.vim;
+  #         };
+  #       }
+  #       )
+  #     ];
+  #   };
 }
