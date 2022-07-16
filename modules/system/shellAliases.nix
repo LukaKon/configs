e464a40c221416599f1549e844773acc2ce817fb
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-07-16 19:53:45 +0200

    mod shell aliases

--- modules/system/shellAliases.nix
+++ modules/system/shellAliases.nix
@@ -1,32 +1,33 @@
-{config, lib, ...}:
+{ config, lib, ... }:
 {
   environment.shellAliases = {
-                                # git
-                                gs = "git status -s";
-                                ga = "git add";
-                                gc = "git commit";
-                                gp = "git push";
-                                gpull = "git pull";
-                                gb = "git branch";
-                                gch = "git checkout";
-                                gm = "git merge";
-                                gf = "git fetch";
+    # git
+    gs = "git status -s";
+    ga = "git add";
+    gc = "git commit";
+    gp = "git push";
+    gpull = "git pull";
+    gb = "git branch";
+    gch = "git checkout";
+    gm = "git merge";
+    gf = "git fetch";
+    glog = "git log --graph --pretty=oneline --abbrev-commit";
 
-                                # term
-                                susp = "systemctl suspend";
-                                reboot = "doas reboot";
+    # term
+    susp = "systemctl suspend";
+    reboot = "doas reboot";
 
-                                # editor
-                                vi = "nvim";
-                                # vi = "vim";
+    # editor
+    vi = "nvim";
+    # vi = "vim";
 
-                                # python
-                                py = "python";
+    # python
+    py = "python";
 
-                                # backup config files to git
-                                cong = "py /home/lk/configs/scripts/git_conf.py";
-    
-                                # docker
-                                dc = "docker container";
-                              };
-                            }
+    # backup config files to git
+    cong = "py /home/lk/configs/scripts/git_conf.py";
+
+    # docker
+    dc = "docker container";
+  };
+}
