67dd933a03424fb7e258c71f073bc05799b61425
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-08-08 10:30:06 +0200

    rm fuji

--- flake.nix
+++ flake.nix
@@ -65,54 +65,6 @@
     rec {
       nixosConfigurations = {
 
-        # desktop
-        # fuji = lib.nixosSystem {
-        #   inherit system pkgs;
-
-        #   modules = [
-        #     # Include the results of the hardware scan.
-        #     ./fuji/hardware-configuration.nix
-
-        #     # System
-        #     ./modules/system/system.nix # Base system settings
-        #     ./modules/system/network.nix # Network configuration
-        #     # ./modules/system/nvidia.nix
-        #     ./modules/system/fonts.nix
-        #     ./modules/system/env_variables.nix
-        #     # ./modules/system/cron.nix
-        #     ./modules/system/shellAliases.nix # Shell aliases
-        #     ./modules/system/lk.nix
-        #     ./modules/system/kk.nix
-
-        #     # Security
-        #     ./modules/security/doas.nix # Add 'doas'
-        #     ./modules/security/firewall.nix
-
-        #     # Services
-        #     ./modules/services/openssh.nix # SSH
-        #     #./modules/services/keyring.nix
-        #     # ./modules/services/bluetooth.nix
-        #     # ./modules/services/remote.nix # remote server/desktop
-
-        #     # Programs
-        #     ./modules/programs/progr.nix
-        #     #./modules/programs/kakoune.nix
-        #     # ./modules/programs/nvim/nvim.nix
-        #     ./modules/programs/zsh.nix
-        #     ./modules/programs/flatpak.nix
-
-        #     # Desktops
-        #     # ./modules/desktops/i3/i3.nix  # i3 window manager settings
-        #     # ./modules/desktops/qtile.nix
-        #     ./modules/desktops/kde.nix
-
-        #     # Virtualisation
-        #     # ./modules/virtualisation/podman.nix # Podman
-        #     # ./modules/virtualisation/docker.nix # Docker
-        #     # ./modules/virtualisation/virt-manager.nix
-        #   ];
-        # };
-
         lap = lib.nixosSystem {
           inherit system pkgs;
 
