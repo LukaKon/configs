4138536873488df7763b65bc82b027a9d0e73b53
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-08-21 13:17:28 +0200

    plying with nix container - nginx

--- containers/test.nix
+++ containers/test.nix
@@ -1,12 +1,39 @@
-{}:
+{ ... }:
 {
   containers.test = {
+    # system.stateVersion = "22.05";
     ephemeral = true;
-    autostart = true;
-    config = { config, pkgs, ...}: {
-      services.httpd.enable = true;
-      services.httpd.adminAddr = "foo@example.org";
+    autoStart = true;
+    config = { config, pkgs, ... }: {
+      services.nginx = {
+        enable = true;
+        # config = ''
+        # server {
+          # listen 80 default_server;
+        #   server_name localhost;
+        
+        #   location / {
+        #     root /usr/share/nginx/html;
+        #     index index.html index.htm;
+        #   }
+        # }
+        # '';
+        # virtualHosts = {
+          # "partywizard.pl" = { };
+        # };
+      };
       networking.firewall.allowedTCPPorts = [ 80 ];
     };
+
+    # bindMounts = {
+    #   "/var/log/httpd" = {
+    #     hostPath = "/mnt/testContainerData/";
+    #     isReadOnly = false;
+    #   };
+    # };
+
+    # privateNetwork = true;
+    # hostAddress = "192.168.0.4";
+    # localAddress = "192.168.0.50";
   };
 }
