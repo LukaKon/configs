6d4b6f806cf8430f8a42139ebac27e56734a4f4d
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-08-23 12:20:47 +0200

    up

--- containers/nginx_pw.nix
+++ containers/nginx_pw.nix
@@ -7,17 +7,17 @@
     config = { config, pkgs, ... }: {
       services.nginx = {
         enable = true;
-        # config = pkgs.lib.readFile ./nginx.conf;
+        config = pkgs.lib.readFile ./nginx.conf;
       };
-      # networking.firewall.allowedTCPPorts = [ 80 ];
+      networking.firewall.allowedTCPPorts = [ 80 ];
     };
 
-    # bindMounts = {
-      # "/usr/share/nginx/html" = {
-        # hostPath = "/home/lk/dev/party_creator/PARTY/front/dist";
-        # isReadOnly = false;
-      # };
-    # };
+    bindMounts = {
+      "/usr/share/nginx/html" = {
+        hostPath = "/home/lk/dev/party_creator/PARTY/front/dist";
+        isReadOnly = false;
+      };
+    };
 
     # privateNetwork = true;
     # hostAddress = "192.168.0.4";
