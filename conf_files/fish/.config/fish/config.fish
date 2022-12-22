9c29b16584d1fb067fa47feab5e40d6b82c2eb32
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-12-22 21:36:42 +0100

    add fish config

---
+++ conf_files/fish/.config/fish/config.fish
@@ -0,0 +1,35 @@
+### EXPORT ###
+set fish_greeting                                 # Supresses fish's intro message
+set TERM "xterm-256color"                         # Sets the terminal type
+
+### SET MANPAGER
+### Uncomment only one of these!
+
+### "bat" as manpager
+#set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+### "nvim" as manpager
+# set -x MANPAGER "nvim -c 'set ft=man' -"
+
+### SET EITHER DEFAULT EMACS MODE OR VI MODE ###
+function fish_user_key_bindings
+  # fish_default_key_bindings
+  fish_vi_key_bindings
+end
+### END OF VI MODE ###
+
+### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
+set fish_color_normal brcyan
+set fish_color_autosuggestion '#7d7d7d'
+set fish_color_command brcyan
+set fish_color_error '#ff6c6b'
+set fish_color_param brcyan
+
+### SETTING THE STARSHIP PROMPT ###
+#starship init fish
+
+
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
