0d7d90b93f5b25f3b63d1082cf5fdfc3de17d0d3
lukakon <lukasz.konieczny.lk@protonmail.com>
2023-02-23 20:11:48 +0100

    spectrwm: add RAM

--- conf_files/spectrwm/.config/spectrwm/baraction.sh
+++ conf_files/spectrwm/.config/spectrwm/baraction.sh
@@ -2,31 +2,23 @@
 # baraction.sh for spectrwm status bar
 
 # ## DISK
-# hdd() {
-#   hdd="$(df -h | awk 'NR==4{print $3, $5}')"
-#   echo -e "HDD: $hdd"
-# }
+hdd() {
+  hdd="$(df -h | awk 'NR==4{print $3, $5}')"
+  echo -e "HDD: $hdd"
+}
 
 # ## RAM
-# mem() {
-#   mem=`free | awk '/Mem/ {printf "%dM/%dM\n", $3 / 1024.0, $2 / 1024.0 }'`
-#   echo -e "$mem"
-# }
+mem() {
+  MEM=`sysctl -n hw.usermem`
+  MEM_GB=$((MEM / 1024000000))
+  echo -e "RAM: $MEM_GB GB"
+}
 
 # ## CPU
 cpu() {
   CPU_FREQ=`sysctl -n dev.cpu.0.freq`
   echo -e "CPU: $CPU_FREQ MHz"
 }
-# cpu() {
-#   read cpu a b c previdle rest < /proc/stat
-#   prevtotal=$((a+b+c+previdle))
-#   sleep 0.5
-#   read cpu a b c idle rest < /proc/stat
-#   total=$((a+b+c+idle))
-#   cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-#   echo -e "CPU: $cpu%"
-# }
 
 # ## BAT
 bat(){
@@ -43,12 +35,6 @@ fi
   echo -e "BAT: $LIFE% $ACTUAL_STATUS"
 }
 
-# ## VOLUME
-# vol() {
-#     vol=`amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }' | sed 's/on://g'`
-#     echo -e "VOL: $vol"
-# }
-
 SLEEP_SEC=5  # set bar_delay = 5 in /etc/spectrwm.conf
 # It seems that we are limited to how many characters can be displayed via
 # the baraction script output. And the the markup tags count in that limit.
@@ -56,8 +42,7 @@ SLEEP_SEC=5  # set bar_delay = 5 in /etc/spectrwm.conf
 # echo output too long to display correctly.
 while :; do
     # echo "+@fg=1; +@fn=1;💻+@fn=0; $(cpu) +@fg=0; | +@fg=2; +@fn=1;💾+@fn=0; $(mem) +@fg=0; | +@fg=3; +@fn=1;💿+@fn=0; $(hdd) +@fg=0; | +@fg=4; +@fn=1;🔈+@fn=0; $(vol) +@fg=0; |"
-    # echo "BAT: $BAT%"
-    echo "$(cpu) $(bat)"
+    echo "$(cpu) $(mem) $(bat)"
 
 	sleep $SLEEP_SEC
 done
