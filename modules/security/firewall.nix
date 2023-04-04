5fe2ce9d6172c640635a9b15caedfc7dd304b8a0
lukakon <lukasz.konieczny.lk@protonmail.com>
2023-04-04 23:00:53 +0200

    mod firewall

--- modules/security/firewall.nix
+++ modules/security/firewall.nix
@@ -1,12 +1,16 @@
 { config, ... }:
 
 {
-  networking.firewall.allowedTCPPorts = [
+  networking.firewall = {
+  enable = true;
+  allowedTCPPorts = [
     3000
     8000
+    8080
     5432
     6379
     80
     443
   ];
+  };
 }
