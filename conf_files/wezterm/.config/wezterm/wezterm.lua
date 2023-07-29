17fb0ad73c706883edb8a62f3435a1ad12994571
lukakon <lukasz.konieczny.lk@protonmail.com>
2023-07-29 16:19:43 +0200

    i3 and wezterm mod

--- conf_files/wezterm/.config/wezterm/wezterm.lua
+++ conf_files/wezterm/.config/wezterm/wezterm.lua
@@ -5,7 +5,15 @@ local config = {}
 config.color_scheme = 'Gruvbox dark, medium (base16)'
 
 -- config.font = wezterm.font('JetBrains Mono', {weight="Bold"})
-config.font = wezterm.font('Fira Code', {weight="Bold", stretch="Normal"})
--- config.font = wezterm.font('Nerd Font', {weight="Bold"})
+-- config.font = wezterm.font({
+--   family = 'Fira Code',
+--   weight = "Black",
+--   stretch = "Normal"
+-- })
+-- config.font = wezterm.font('SauceCodePro Nerd Font', {weight="Regular"})
+config.font = wezterm.font('Overpass Regular Nerd Font Complete', { weight = "Regular" })
+-- config.font = wezterm.font('Overpass Nerd Font Mono', {weight="Regular"})
+
+config.window_background_opacity = 0.8
 
 return config
