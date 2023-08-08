107a3ed5e899fd4090b759734c70139d9117dd8a
lukakon <lukasz.konieczny.lk@protonmail.com>
2023-08-08 23:08:44 +0200

    wezterm conf mod

--- conf_files/wezterm/.config/wezterm/wezterm.lua
+++ conf_files/wezterm/.config/wezterm/wezterm.lua
@@ -5,11 +5,12 @@ local config = {}
 config.color_scheme = 'Gruvbox dark, medium (base16)'
 
 -- config.font = wezterm.font('JetBrains Mono', {weight="Bold"})
-config.font = wezterm.font({
-  family = 'Fira Code',
-  weight = "Regular",
-  stretch = "Normal"
-})
+-- config.font = wezterm.font({
+--   family = 'Fira Code',
+--   weight = "Regular",
+--   stretch = "Normal"
+-- })
+config.font = wezterm.font({family = 'Symbols Nerd Font', weight = 'Regular'})
 -- config.font = wezterm.font('SauceCodePro Nerd Font', {weight="Regular"})
 -- config.font = wezterm.font('Overpass Regular Nerd Font Complete', { weight = "Regular" })
 -- config.font = wezterm.font('Overpass Nerd Font Mono', {weight="Regular"})
@@ -17,5 +18,7 @@ config.font = wezterm.font({
 config.window_background_opacity = 0.8
 
 config.tab_max_width = 16
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
