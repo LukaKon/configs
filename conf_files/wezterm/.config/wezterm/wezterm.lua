b90eb1dfe74bd77d2d354495548771b93e11192d
lukakon <lukasz.konieczny.lk@protonmail.com>
2023-08-09 22:01:00 +0200

    switch to wezterm

--- conf_files/wezterm/.config/wezterm/wezterm.lua
+++ conf_files/wezterm/.config/wezterm/wezterm.lua
@@ -5,20 +5,50 @@ local config = {}
 config.color_scheme = 'Gruvbox dark, medium (base16)'
 
 -- config.font = wezterm.font('JetBrains Mono', {weight="Bold"})
--- config.font = wezterm.font({
---   family = 'Fira Code',
---   weight = "Regular",
---   stretch = "Normal"
--- })
-config.font = wezterm.font({family = 'Symbols Nerd Font', weight = 'Regular'})
--- config.font = wezterm.font('SauceCodePro Nerd Font', {weight="Regular"})
--- config.font = wezterm.font('Overpass Regular Nerd Font Complete', { weight = "Regular" })
--- config.font = wezterm.font('Overpass Nerd Font Mono', {weight="Regular"})
+config.font = wezterm.font({
+  -- family = 'Inconsolata Nerd Font Mono',
+  -- family = 'Iosevka Nerd Font Mono',
+  -- family = 'FiraCode Nerd Font Mono',
+  family = 'FiraCode Nerd Font Mono',
+  weight = "Regular",
+  stretch = "Normal",
+  style = 'Normal',
+})
+config.font_size = 12.0
 
 config.window_background_opacity = 0.8
 
 config.tab_max_width = 16
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = true
+
+config.leader = {
+  key = 'a',
+  mods = 'CTRL',
+  timeout_milliseconds = 1000,
+}
+config.keys = {
+  {
+    key = '\\',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '-',
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'w',
+    mods = 'LEADER',
+    action = wezterm.action.CloseCurrentTab { confirm = true },
+  },
+  {
+    key = 'P',
+    mods = 'CTRL',
+    action = wezterm.action.ActivateCommandPalette,
+  },
+}
 
 return config
