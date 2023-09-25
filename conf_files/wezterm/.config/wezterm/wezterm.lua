9d7e0c1a7cb9171edf317e49db231381cbca0867
lukakon <lukasz.konieczny.lk@protonmail.com>
2023-09-25 18:00:48 +0200

    wezterm mod

--- conf_files/wezterm/.config/wezterm/wezterm.lua
+++ conf_files/wezterm/.config/wezterm/wezterm.lua
@@ -5,6 +5,10 @@ local config = {}
 
 if wezterm.config_builder then config = wezterm.config_builder() end
 
+wezterm.on('update-right-status', function(window, pane)
+  window:set_right_status(window:active_workspace())
+end)
+
 config.color_scheme = 'Gruvbox dark, medium (base16)'
 
 config.font = wezterm.font_with_fallback({
@@ -50,6 +54,79 @@ config.keys = {
   { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
   { key = "s", mods = "LEADER", action = act.RotatePanes "Clockwise" },
   { key = 'P', mods = 'CTRL', action = wezterm.action.ActivateCommandPalette, },
+
+  -- Switch to default workspace
+  {
+    key = 'y',
+    mods = 'CTRL|SHIFT',
+    action = act.SwitchToWorkspace {
+      name = 'default',
+    },
+  },
+  -- Switch to a monitoring workspace
+  {
+    key = 'u',
+    mods = 'CTRL|SHIFT',
+    action = act.SwitchToWorkspace {
+      name = 'monitoring',
+      spawn = {
+        args = { 'btop' },
+      },
+    },
+  },
+  -- Create a new workspace with a random name and switch to it
+  { key = 'i', mods = 'CTRL|SHIFT', action = act.SwitchToWorkspace },
+  -- Show the launcher in fuzzy selection mode and have it list all workspaces
+  -- and allow activating one.
+  {
+    key = '9',
+    mods = 'ALT',
+    action = act.ShowLauncherArgs {
+      flags = 'FUZZY|WORKSPACES',
+    },
+  },
+  -- Prompt for a name to use for a new workspace and switch to it.
+  {
+    key = 'W',
+    mods = 'CTRL|SHIFT',
+    action = act.PromptInputLine {
+      description = wezterm.format {
+        { Attribute = { Intensity = 'Bold' } },
+        { Foreground = { AnsiColor = 'Fuchsia' } },
+        { Text = 'Enter name for new workspace' },
+      },
+      action = wezterm.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:perform_action(
+            act.SwitchToWorkspace {
+              name = line,
+            },
+            pane
+          )
+        end
+      end),
+    },
+  },
+  {
+    key = '9',
+    mods = 'ALT',
+    action = act.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' },
+  },
+  { key = 'n', mods = 'CTRL', action = act.SwitchWorkspaceRelative(1) },
+  { key = 'p', mods = 'CTRL', action = act.SwitchWorkspaceRelative(-1) },
+  {
+    key = 'n',
+    mods = 'SHIFT|CTRL',
+    action = wezterm.action.ToggleFullScreen,
+  },
+  {
+    key = 'Z',
+    mods = 'CTRL',
+    action = wezterm.action.TogglePaneZoomState,
+  },
 }
 
 return config
