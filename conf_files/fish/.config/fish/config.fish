9413622782134d6aab219a225279f6a07b389446
lukakon <lukasz.konieczny.lk@protonmail.com>
2023-10-03 19:49:39 +0200

    fish: playing with fzf - repleace find with fd

--- conf_files/fish/.config/fish/config.fish
+++ conf_files/fish/.config/fish/config.fish
@@ -30,7 +30,7 @@ set -gx DISPLAY :0
 set -gx FZF_DEFAULT_OPTS "--layout reverse --height=15 --extended --multi --cycle --border rounded --prompt='▶' --pointer='' --marker='' --color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54"
 
 # set -gx FZF_DEFAULT_COMMAND "fd --type f --color=never --hidden"
-set -gx FZF_DEFAULT_COMMAND 'find . \! -name ".git/"'
+set -gx FZF_DEFAULT_COMMAND 'fd . --exclude .git --exclude node_modules'
 
 #set -gx FZF_CTRL_T_COMMAND "
 #  --preview 'bat -n --color=always {}'
