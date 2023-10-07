b4c4431865adbebe87968bc07454531677c08adf
lukakon <lukasz.konieczny.lk@protonmail.com>
2023-10-07 19:52:03 +0200

    rm api_key

--- conf_files/fish/.config/fish/config.fish
+++ conf_files/fish/.config/fish/config.fish
@@ -26,8 +26,6 @@ set -gx EDITOR hx
 set -gx BROWSER firefox
 set -gx DISPLAY :0
 
-set -gx OPENAI_KEY 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
-
 # fzf
 set -gx FZF_DEFAULT_OPTS "--layout reverse --height=50% --extended --multi --cycle --border rounded --prompt='▶' --pointer='' --marker='' --color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54"
 
