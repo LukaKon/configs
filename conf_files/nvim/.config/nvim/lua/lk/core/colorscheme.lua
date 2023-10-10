d9bad0e43dc4f94522992c650e3a8860edd49c69
lukakon <lukasz.konieczny.lk@protonmail.com>
2023-10-10 20:20:49 +0200

    nvim change colorscheme to gruvbox

--- conf_files/nvim/.config/nvim/lua/lk/core/colorscheme.lua
+++ conf_files/nvim/.config/nvim/lua/lk/core/colorscheme.lua
@@ -1,4 +1,3 @@
--- local status, nightfox = pcall(require, 'nightfox')
 local status, gruvbox = pcall(require,'gruvbox')
 if(not status)
 then
@@ -32,25 +31,3 @@ gruvbox.setup({
 })
 
 vim.cmd('colorscheme gruvbox')
--- nightfox.setup({
---   palettes = {
---     nordfox = {
---       -- A palette also defines the following:
---       --   bg0, bg1, bg2, bg3, bg4, fg0, fg1, fg2, fg3, sel0, sel1, comment
---       --
---       -- These are the different foreground and background shades used by the theme.
---       -- The base bg and fg is 1, 0 is normally the dark alternative. The others are
---       -- incrementally lighter versions.
---       bg1 = "#2e3440",
-
---       -- sel is different types of selection colors.
---       sel0 = "#3e4a5b", -- Popup bg, visual selection bg
---       sel1 = "#4f6074", -- Popup sel bg, search bg
-
---       -- comment is the definition of the comment color.
---       comment = "#60728a",
---     },
---   },
--- })
-
--- vim.cmd('colorscheme nightfox')
