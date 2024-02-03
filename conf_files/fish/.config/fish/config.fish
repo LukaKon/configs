608acbf0f1adac7e776e8cf34ddad0173e5f3ac0
lukakon <lukasz.konieczny.lk@protonmail.com>
2024-02-03 20:21:42 +0100

    fish: mod

--- conf_files/fish/.config/fish/config.fish
+++ conf_files/fish/.config/fish/config.fish
@@ -111,13 +111,12 @@ alias fu="dsh lk@192.168.1.103"
 alias na="dsh lk@192.168.1.112"
 
 # NFS
-alias media="doas mount -t nfs 192.168.1.103:/data/photo /mnt/photo ;
-doas mount -t nfs 192.168.1.103:/data/music /mnt/music ;
-doas mount -t nfs 192.168.1.103:/data/audiobook /mnt/audiobook ;
-doas mount -t nfs 192.168.1.103:/data/dev /mnt/dev ;
+alias media="doas mount -t nfs 192.168.1.103:/tera/data/photos /mnt/photos ;
+doas mount -t nfs 192.168.1.103:/tera/data/audio/ /mnt/music ;
+doas mount -t nfs 192.168.1.103:/tera/data/dev /mnt/dev ;
 doas mount -t nfs 192.168.1.103:/tera/exports /mnt/exports"
 alias nas="doas mount -t nfs 192.168.1.112:/volume2/export /mnt/nas"
-alias um="doas umount /mnt/nas /mnt/photo /mnt/audiobook /mnt/music /mnt/dev /mnt/exports"
+alias um="doas umount /mnt/nas /mnt/photo /mnt/music /mnt/dev /mnt/exports"
 
 ### SET MANPAGER
 ### Uncomment only one of these!
