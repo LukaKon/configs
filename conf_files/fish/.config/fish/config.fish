49d466e93d8f22c6140612d54b6fc7562b5031d5
lukakon <lukasz.konieczny.lk@protonmail.com>
2024-05-22 19:49:36 +0200

    mod

--- conf_files/fish/.config/fish/config.fish
+++ conf_files/fish/.config/fish/config.fish
@@ -23,7 +23,8 @@ set -g theme_hide_hostname no
 set -g theme_hostname always
 
 set -gx EDITOR hx
-set -gx BROWSER firefox
+# set -gx BROWSER firefox
+set -gx BROWSER librewolf
 set -gx DISPLAY :0
 # set -gx QT_QPA_PLATFORMTHEME qt5ct
 # set SSH_AUTH_SOCK /tmp/ssh-Zek7BjyxuEDg/agent.79284
