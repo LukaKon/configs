1fab444fcd3a12ac44e060fe4e6a3093cb7eefd5
lukakon <lukasz.konieczny.lk@protonmail.com>
2024-07-08 18:45:20 +0200

    playing with spectrwm

--- conf_files/spectrwm/.config/spectrwm/baraction.sh
+++ conf_files/spectrwm/.config/spectrwm/baraction.sh
@@ -3,43 +3,47 @@
 
 # ## DISK
 hdd() {
-  hdd="$(df -h | awk 'NR==4{print $3, $5}')"
-  echo -e "HDD: $hdd"
+  # hdd="$(df -h | awk 'NR==4{print $3, $5}')"
+  # echo -e "HDD: $hdd"
+  echo -e "HDD"
 }
 
 # ## RAM
 mem() {
-  MEM=`sysctl -n hw.usermem`
-  MEM_GB=$((MEM / 1024000000))
-  echo -e "RAM: $MEM_GB GB"
+  # MEM=`sysctl -n hw.usermem`
+  # MEM_GB=$((MEM / 1024000000))
+  # echo -e "RAM: $MEM_GB GB"
+  echo -e "RAM"
 }
 
 # ## CPU
 cpu() {
-  CPU_FREQ=`sysctl -n dev.cpu.0.freq`
-  echo -e "CPU: $CPU_FREQ MHz"
+  # CPU_FREQ=`sysctl -n dev.cpu.0.freq`
+  # echo -e "CPU: $CPU_FREQ MHz"
+  echo -e "CPU"
 }
 
 # ## BAT
 bat(){
-  IS_BATTERY=`sysctl -n hw.acpi.battery`
+  # IS_BATTERY=`sysctl -n hw.acpi.battery`
 
-  if [ -z $IS_BATTERY ]
-    then
-      echo -e "no battery"
-    else
-      LIFE=`sysctl -n hw.acpi.battery.life`
-      STATUS=`sysctl -n hw.acpi.battery.state`
+  # if [ -z $IS_BATTERY ]
+  #   then
+  #     echo -e "no battery"
+  #   else
+  #     LIFE=`sysctl -n hw.acpi.battery.life`
+  #     STATUS=`sysctl -n hw.acpi.battery.state`
 
-      if [ $STATUS -eq 1 ]
-      then
-        ACTUAL_STATUS="discharge"
-      else
-        ACTUAL_STATUS="charge"
-      fi
+  #     if [ $STATUS -eq 1 ]
+  #     then
+  #       ACTUAL_STATUS="discharge"
+  #     else
+  #       ACTUAL_STATUS="charge"
+  #     fi
 
-      echo -e "BAT: $LIFE% $ACTUAL_STATUS"
-  fi
+  #     echo -e "BAT: $LIFE% $ACTUAL_STATUS"
+  # fi
+  echo -e "BAT"
 }
 
 SLEEP_SEC=3  # set bar_delay = 5 in /etc/spectrwm.conf
@@ -48,7 +52,9 @@ SLEEP_SEC=3  # set bar_delay = 5 in /etc/spectrwm.conf
 # So I would love to add more functions to this script but it makes the 
 # echo output too long to display correctly.
 while :; do
-    echo "$(cpu) $(mem) $(bat)"
+    # echo "$(cpu) $(mem) $(bat)"
+    echo "TEST"
+
 
 	sleep $SLEEP_SEC
 done
