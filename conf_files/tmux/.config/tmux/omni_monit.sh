e526fe43266dcd2acdea43f2ec9acef596b3f5bb
lukakon <lukasz.konieczny.lk@protonmail.com>
2024-09-29 13:13:45 +0200

    tmux layout for monitoring omni

---
+++ conf_files/tmux/.config/tmux/omni_monit.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+NAME='monitoring'
+
+# start a new tmux session or attach to existing one
+tmux new-session -d -s $NAME
+
+# first window with helix editor run
+tmux rename-window -t $NAME:0 'htop' # rename the first window
+tmux send-keys -t $NAME:0 'htop' C-m # run application in the first window
+
+# create second window
+tmux new-window -t $NAME:1 -n 'proc/disk'
+
+# split second window into panes
+tmux split-window -h -p 50 # create vertical split (70% left)
+# tmux split-window -v -p 50 # split right pane horizontally (50% of remaining)
+
+# naming panes
+tmux select-pane -t $NAME:1.0
+tmux select-pane -T 'prstat'
+
+tmux select-pane -t $NAME:1.1
+tmux select-pane -T 'zpool'
+
+# run applications/commands in panes
+tmux send-keys -t $NAME:1.0 'prstat -Z' C-m
+tmux send-keys -t $NAME:1.1 'zpool iostat -v 2' C-m
+
+# attach to the session
+tmux attach-session -t $NAME
