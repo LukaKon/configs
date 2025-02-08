dcf2c159feae2e0c2aa5deacecd30de6de5e7793
lukakon <lukasz.konieczny.lk@protonmail.com>
2025-02-08 22:22:42 +0100

    add appjail

--- conf_files/fish/.config/fish/config.fish
+++ conf_files/fish/.config/fish/config.fish
@@ -132,8 +132,8 @@ alias na="dsh lk@192.168.1.112"
 # NFS
 # alias media="doas mount -t nfs 192.168.1.103:/export/photos /mnt/photos ;
 # doas mount -t nfs 192.168.1.103:/export/coding /mnt/dev"
-alias nas="doas mount -t nfs 192.168.1.112:/volume2/export /mnt/nas"
-alias um="doas umount /mnt/nas /mnt/photos /mnt/dev /mnt/exports"
+alias nas="doas mount -t nfs 192.168.1.112:/volume1/export /mnt/nas"
+alias um="doas umount /mnt/nas" # /mnt/photos /mnt/dev /mnt/exports"
 
 ### SET MANPAGER
 ### Uncomment only one of these!
