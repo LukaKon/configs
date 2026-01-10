5450b5ad29fec122536f1292f9a4388ffcc51e3a
lukakon <lukasz.konieczny.lk@protonmail.com>
2026-01-10 14:36:14 +0100

    mod fish prompt

--- conf_files/fish/.config/fish/functions/fish_prompt.fish
+++ conf_files/fish/.config/fish/functions/fish_prompt.fish
@@ -1,48 +1,44 @@
-function fish_prompt --description 'Gruvbox Dark one-line prompt'
-    # Gruvbox Dark colors
-    set normal (set_color normal)
-    set bold (set_color --bold)
-    set fg0  (set_color fb4934)  # Red
-    set fg1  (set_color b8bb26)  # Green  
-    set fg2  (set_color fabd2f)  # Yellow
-    set fg3  (set_color 83a598)  # Aqua
-    set fg4  (set_color d3869b)  # Purple
-    set fg5  (set_color 8ec07c)  # Green2
-    set fg6  (set_color fe8019)  # Orange
-    set fg9  (set_color ebddb2)  # Light fg
+function fish_prompt
+    set -l normal (set_color normal)
+    set -l bold (set_color --bold)
+    set -l fg0 (set_color fb4934) # Red
+    set -l fg1 (set_color b8bb26) # Green  
+    set -l fg2 (set_color fabd2f) # Yellow
+    set -l fg3 (set_color 83a598) # Aqua
+    set -l fg4 (set_color d3869b) # Purple
+    set -l fg5 (set_color 8ec07c) # Green2
+    set -l fg6 (set_color fe8019) # Orange
+    set -l fg9 (set_color ebddb2) # Light fg
 
-    # Czas (FreeBSD)
-    # printf '%s' $fg3(date "+%H:%M:%S") $normal ' '
-    
-    # User@Host - POPRAWIONE
-    printf '%s' $bold$fg9$USER$normal '@' $fg5(hostname | cut -d. -f1) ' '
-    
+    set -l last_status $status
+
+    set -l delim $fg6" > "$normal
+    fish_is_root_user; and set delim $fg0" # "$normal
+
+    set -l prompt_status #$fg5" ok"
+    # TODO: not working when $status is greater than 0
+    test $last_status -ne 0; and set prompt_status $fg0" [$last_status]"$normal
+
+    # switch $last_status
+    #     case 0
+    #         set prompt_status $fg5" ok"
+    #     case '*'
+    #         set prompt_status $fg0" [$last_status] "$normal
+    # end
+
     # SSH IP
     if set -q SSH_CONNECTION
-        set ssh_ip (echo $SSH_CONNECTION | awk '{print $3}')
-        printf '%s' $fg3"[$ssh_ip] "
+        set ssh_ip $fg2"|"(echo $SSH_CONNECTION | awk '{print $3}')"|"
     end
-    
-    # Exit status
-    if test $status -ne 0
-        printf '%s' $fg0"✘$status "
-    else
-        printf '%s' $fg1'✔ '
-    end
-    
-    # Katalog + Git
-    printf '%s' $fg2(prompt_pwd)
-    if git rev-parse --git-dir > /dev/null 2>&1
-        set git_branch (git symbolic-ref --short HEAD 2>/dev/null; or git rev-parse --short HEAD)
-        set git_status (git status --porcelain 2>/dev/null | wc -l)
-        printf ' ' $fg4"git:" $fg3"($git_branch)"
-        test $git_status -gt 0 && printf ' ' $fg6"●"
-    end
-    
-    # CMD duration
-    if test "$CMD_DURATION -gt 2000"
-        printf '%s%s%.1fs' $fg0 '⏱' (math "$CMD_DURATION / 1000")
-    end
-    
-    printf '%s' $normal ' > '
+
+    set user $bold$fg9$USER$normal
+    fish_is_root_user; and set user $bold$fg0$USER$normal
+
+    set prompt_host $user$fg4@$fg5$hostname$normal
+
+    # Shorten pwd if prompt is too long
+    # set -l pwd " "$fg6(prompt_pwd)
+    set -l pwd $fg6" ("(basename $PWD)")"
+
+    echo -n -s $prompt_host $ssh_ip $pwd $normal $prompt_status $delim
 end
--- conf_files/fish/.config/fish/functions/fish_right_prompt.fish
+++ conf_files/fish/.config/fish/functions/fish_right_prompt.fish
@@ -1,32 +1,43 @@
 function fish_right_prompt
+        set -l normal (set_color normal)
+        set -l bold (set_color --bold)
+        set -l fg0 (set_color fb4934) # Red
+        set -l fg1 (set_color b8bb26) # Green  
+        set -l fg2 (set_color fabd2f) # Yellow
+        set -l fg3 (set_color 83a598) # Aqua
+        set -l fg4 (set_color d3869b) # Purple
+        set -l fg5 (set_color 8ec07c) # Green2
+        set -l fg6 (set_color fe8019) # Orange
+        set -l fg9 (set_color ebddb2) # Light fg
+
         set -g __fish_git_prompt_showdirtystate 1
         set -g __fish_git_prompt_showuntrackedfiles 1
         set -g __fish_git_prompt_showupstream informative
         set -g __fish_git_prompt_showcolorhints 1
         set -g __fish_git_prompt_use_informative_chars 1
+
         # Unfortunately this only works if we have a sensible locale
         string match -qi "*.utf-8" -- $LANG $LC_CTYPE $LC_ALL
-        and set -g __fish_git_prompt_char_dirtystate \U1F4a9
+        and set -g __fish_git_prompt_char_dirtystate "dirty"
         set -g __fish_git_prompt_char_untrackedfiles "?"
     
         # The git prompt's default format is ' (%s)'.
         # We don't want the leading space.
-        set -l vcs (fish_vcs_prompt '(%s)' 2>/dev/null)
+        set -l vcs (fish_vcs_prompt '(%s)' 2>/dev/null)" "
     
-        set -l d (set_color brgrey)(date "+%R")(set_color normal)
+        set -l time $fg1(date "+%R")$normal
     
-        # set -l duration "$cmd_duration$CMD_DURATION"
-        # if test $duration -gt 100
-                # set duration (math $duration / 1000)s
-        # else
-                # set duration
-        # end
+        set -l duration "$cmd_duration$CMD_DURATION"
+        if test $duration -gt 100
+                set duration $fg0(math $duration / 1000)s$normal" "
+        else
+                set duration
+        end
     
         set -q VIRTUAL_ENV_DISABLE_PROMPT
         or set -g VIRTUAL_ENV_DISABLE_PROMPT true
         set -q VIRTUAL_ENV
-        and set -l venv (string replace -r '.*/' '' -- "$VIRTUAL_ENV")
-    
-        set_color normal
-        string join " " -- $venv $duration $vcs $d
+        and set -l venv $fg4(string replace -r '.*/' '' -- "$VIRTUAL_ENV")$normal
+
+        echo -n -s $duration $venv $vcs $time
 end
